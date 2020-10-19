677e0525cd7adda838409fba380622d350bb8dd3
Yeshin Lee <yeslee5004@gmail.com>
2020-10-19 22:27:25 +0900

    modify ft_split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 21:37:05 by yeslee            #+#    #+#             */
-/*   Updated: 2020/10/19 21:37:54 by yeslee           ###   ########.fr       */
+/*   Updated: 2020/10/19 22:26:58 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,30 @@
 
 char	**ft_split(char const *s, char c)
 {
+	size_t	i;
+	size_t	cnt;
+	char	**all;
+	char	*floor;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		cnt = 0;
+		if (s[cnt] != c)
+			cnt++;
+		else
+		{
+			if (!(floor = malloc(cnt + 1)))
+				return (0);
+			while (i < cnt)
+			{
+				floor[i] = s[i];
+				i++;
+			}
+			floor[i] = '\0';
+		}
+	}
+	return (all);
 }
