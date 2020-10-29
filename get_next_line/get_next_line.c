6de33b8b431a316e5540b8c094e420ddc6ed2706
Yeshin Lee <yeslee5004@gmail.com>
2020-10-29 21:16:08 +0900

    modify

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,111 +6,47 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 20:47:55 by yeslee            #+#    #+#             */
-/*   Updated: 2020/10/28 22:01:41 by yeslee           ###   ########.fr       */
+/*   Updated: 2020/10/29 21:14:55 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "get_next_line.h"
 
-char	*ft_strcpy(char *dest, const char *src)
-{
-	unsigned int i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = src[i];
-	return (dest);
-}
-
-char	*ft_strnew(size_t size)
-{
-	char	*str;
-
-	if (!(str = (char *)malloc(sizeof(char) * (size + 1))))
-		return (0);
-	str[size] = '\0';
-	while (size--)
-		str[size] = '\0';
-	return (str);
-}
-
-void	ft_strclr(char *s)
-{
-	if (s)
-	{
-		while (*s)
-		{
-			*s = '\0';
-			s++;
-		}
-	}
-}
-
-char	*check_remainder(char *remainder, char **line)
-{
-	char	*p_n;
-
-	p_n = NULL;
-	if (remainder)
-	{
-		if ((p_n = ft_strchr(remainder, '\n')))
-		{
-			*p_n = '\0';
-			*line = ft_strdup(remainder);
-			p_n++;
-			ft_strcpy(remainder, p_n);
-		}
-		else
-		{
-			*line = ft_strdup(remainder);
-			ft_strclr(remainder);
-		}
-	}
-	else
-	{
-		*line = ft_strnew(1);
-	}
-	return (p_n);
-}
-
 int		get_next_line(int fd, char **line)
 {
-	char	buf[BUFFER_SIZE + 1];
-	int		byte_was_read;
-	char	*p_n;
-	static char	*remainder;
-	char	*tmp;
+	static char	*isremain; // check '\n'
+	char		buf[BUFFER_SIZE + 1];
+	char		*tmp;
 
-	p_n = check_remainder(remainder, line);
-	while (!p_n && (byte_was_read = read(fd, buf, BUFFER_SIZE)))
+	if (fd < 0 || !line)
+		return (-1);
+	if (!(tmp = malloc(sizeof(char) + 1)))
+		return (-1);
+	while (read(fd, buf, BUFFER_SIZE) != -1)
 	{
-		buf[byte_was_read] = '\0';
-		if ((p_n = ft_strchr(buf, '\n')))
-		{
-			*p_n = '\0';
-			p_n++;
-			remainder = ft_strdup(p_n);
-		}
-		tmp = *line;
-		*line = ft_strjoin(*line, buf);
-		free(tmp);
+		tmp = ft_strjoin(tmp, buf);
+		if (ft_strchr(buf, '\n'))
+			break ;
 	}
-	return (byte_was_read || ft_strlen(remainder) || ft_strlen(*line)) ? 1 : 0;
+	while (*tmp != '\n')
+		**line++ = tmp++;
+	isremain = ft_strdup(tmp + 1);
+	
 }
-/*
+
 int main(void)
 {
-	char	*line;
-	int		fd;
-
-	fd = open("test.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("1. %s\n\n", line);
-	get_next_line(fd, &line);
-	printf("2. %s\n", line);
-}*/
+	char *line = 0;
+	int ret;
+	int fd;
+	fd = open("your_file_name", O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
+	return (0);
+}
