9792379cfdac795e4c7d7fa6d32c8189e0f97d82
Yeshin Lee <yeslee5004@gmail.com>
2020-11-12 16:00:04 +0900

    test %d

---
+++ ft_printf/main.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 15:03:51 by yeslee            #+#    #+#             */
+/*   Updated: 2020/11/12 15:59:27 by yeslee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+
+int	ft_printf(const char *str, ...);
+
+int main()
+{
+	printf("%d\n", 123);
+//	ft_printf("%d\n", 123);
+	
+/*	printf("%8d\n", 123);
+//	ft_printf("%8d\n", 123);
+
+	printf("%-8d|\n", 123);
+//	ft_printf("%-8d\n", 123);
+	
+	printf("%+8d\n", 123);
+//	ft_printf("%+8d\n", 123);
+	
+	printf("% 8d\n", 123);
+//	ft_printf("% 8d\n", 123);
+	
+	printf("%08d\n", 123);
+//	ft_printf("%08d\n", 123);
+
+	printf("%-+8d\n", 123);
+//	ft_printf("%-+8d\n", 123);
+	
+	printf("%- 8d\n", 123);
+//	ft_printf("%- 8d\n", 123);
+
+//	printf("%-08d\n", 123);	// flag '0' is ignored
+	
+	printf("%+08d\n", 123);
+//	ft_printf("%+08d\n", 123);
+	
+	printf("% 08d\n", 123);
+//	ft_printf("% 08d\n", 123);
+
+	printf("%.5d\n", 123);
+*/	
+	return (0);
+}
