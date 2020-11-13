2b4d25f300e1ca13f27b12e981b5e7238c7b8734
Yeshin Lee <yeslee5004@gmail.com>
2020-11-13 20:53:57 +0900

    delete remarks

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 22:11:57 by yeslee            #+#    #+#             */
-/*   Updated: 2020/11/13 19:53:34 by yeslee           ###   ########.fr       */
+/*   Updated: 2020/11/13 20:53:26 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,21 @@ int	ft_printf(const char *str, ...)
 	va_list	ap;
 
 	va_start(ap, str);
-	//printf("%s", str);	// %d
 	while (*str)
 	{
 		if (*str != '%')
-			//printf("%c", *str);
 			write(1, &(*str), 1);
-		else // *str == %
+		else
 		{
-				//printf("*");
 			if (*(str + 1) == 'd')
 			{
 				result = va_arg(ap, int);
 				write(1, ft_itoa(result), ft_strlen(ft_itoa(result)));
-				//printf("%d", result);
 				str++;
 			}
 		}
 		str++;
 	}
 	va_end(ap);
-	//printf("%d\n", result);
 	return (result);
 }
