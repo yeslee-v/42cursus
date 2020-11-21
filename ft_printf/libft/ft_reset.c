de79da655b568ae80a301171ac30cbbd7b599734
Yeshin Lee <yeslee5004@gmail.com>
2020-11-21 14:08:06 +0900

    error ft_reset,c

--- ft_printf/libft/ft_reset.c
+++ ft_printf/libft/ft_reset.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 14:42:58 by yeslee            #+#    #+#             */
-/*   Updated: 2020/11/21 12:29:24 by yeslee           ###   ########.fr       */
+/*   Updated: 2020/11/21 14:03:15 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 // %d%f 맨 처음 초기화 + 다음 % 가 나왔을 때 몇몇 인자들 초기화
 
-int	ft_reset(t_list lst)
+void	ft_reset()
 {
-	if (!lst)
-		return (0);
-	else
-	{
+	t_list	lst;
+
+//	if (!lst)
+//		return ;
+//	else
+//	{
 		//t_list.space = 0;
 		//t_list.plus = 0;
-		t_list.left = '0';
+		t_listeft = '0';
 		t_list.zero = '0';
 		//t_list.sharp = 0;
 
@@ -35,6 +37,6 @@ int	ft_reset(t_list lst)
 
 		t_list.result = 0;
 		t_list.cnt = 0;
-	}
-	return (0);
+//	}
+	return ;
 }
