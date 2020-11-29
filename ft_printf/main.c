8b1a6096c9de3f6dbf4409f77b55be83fdb49973
Yeshin Lee <yeslee5004@gmail.com>
2020-11-29 21:36:48 +0900

    remove leaks

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 15:03:51 by yeslee            #+#    #+#             */
-/*   Updated: 2020/11/22 18:02:51 by yeslee           ###   ########.fr       */
+/*   Updated: 2020/11/29 21:36:35 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,5 @@ int main()
 	//printf("%04d\n", 123);
 	ft_printf("hello\n%---123.123d\nworld\n%2.12f\n", 123, 10);
 
-//	printf("hello%---12.23d\n world%23.12d\n", 123, 123);
-/* memory leak check
-	ft_printf("%d %c %s %i %u %x %X %p\n", 1, 10, 'c', "hello", 1, 1, 1, 1, NULL);
-	while (1)
-		;
-*/
 	return (0);
 }
