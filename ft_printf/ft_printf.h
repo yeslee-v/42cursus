5b33ede41607a0935401266d379dfb39a43f55b7
Yeshin Lee <yeslee5004@gmail.com>
2021-01-03 23:03:07 +0900

    Add ft_len

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,41 +6,38 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 22:12:53 by yeslee            #+#    #+#             */
-/*   Updated: 2020/12/03 16:49:49 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/03 22:27:49 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#define FT_PRINTF_H
 
-# include <stdarg.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-typedef	struct	s_list {
-//	int		space;
-//	char	plus;
-	int		left;
-	int		zero;
-//	char	sharp;
+typedef struct	s_list
+{
+	int			left;
+	int			zero;
+	int			width;
+	int			result;
 
-	int		width;
-	int		prec;	// precision
-
-	char	type;	// cspdiuxX%
-
-	int		len;	// length of va_arg(ap, int)
-	//char	*value;	
-
-	int		result;
-	int		cnt;	// printf("%d\n", printf("%d\n", 123));
+	//	int		space;
+	//	char	plus;
+	//	char	sharp;
+	int prec;  // precision
+	char type; // cspdiuxX%
+	int len;   // length of va_arg(ap, int)
+	// char	*value;
+	int cnt; // printf("%d\n", printf("%d\n", 123));
 }				t_list;
-
-int	    ft_printf(const char *str, ...);
-const char  *ft_parsing(const char *str);
-char	    ft_type(char c);
-int	    ft_atoi(char *str);
-void	    ft_flag_d(t_list *lst);
-char	    *ft_itoa(int n);
-
+int				ft_printf(const char *str, ...);
+const char		*ft_parsing(const char *str);
+char			ft_type(char c);
+int				ft_atoi(char *str);
+void			ft_flag_d(t_list *lst);
+char			*ft_itoa(int n);
+int				ft_len(int n);
 #endif
