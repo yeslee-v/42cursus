01587842096916d308fbb9c8815ade0d10ff0c20
Yeshin Lee <yeslee5004@gmail.com>
2021-01-04 12:05:06 +0900

    Try again

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,26 +6,40 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 15:03:51 by yeslee            #+#    #+#             */
-/*   Updated: 2020/12/03 22:04:07 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/04 11:55:46 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 #include <stdlib.h>
-int	ft_printf(const char *str, ...);
-void	ft_flag(t_list lst);
 
 int main()
 {
-/*	printf("01. -%d\n", 123);
-	ft_printf("01. -%d\n\n", 123);
-	
-	printf("02. %-d\n", 123);
-	ft_printf("02. %-d\n\n", 123);*/
+    printf("Result of applying conversion to 123\n");
+    
+	printf(  "%8d\n", 123);
+    ft_printf(  "%8d\n", 123);
+    
+/*	printf( "%-8d\n", 123);
+    printf( "%+8d\n", 123);
+    printf( "% 8d\n", 123);
+    printf( "%08d\n", 123);
+    printf("%-+8d\n", 123);
+    printf("%- 8d\n", 123);
+    printf("%+08d\n", 123);
+    printf("% 08d\n", 123);
 
-	ft_printf("%-10d", 12);	//d0567 
-	ft_printf("%04d", 567);
-
+    printf("Result of applying conversion to -123\n");
+    printf(  "%8d\n", -123);
+    printf( "%-8d\n", -123);
+    printf( "%+8d\n", -123);
+    printf( "% 8d\n", -123);
+    printf( "%08d\n", -123);
+    printf("%-+8d\n", -123);
+    printf("%- 8d\n", -123);
+    printf("%+08d\n", -123);
+    printf("% 08d\n", -123);
+*/
 	return (0);
 }
