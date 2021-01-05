202d280880436801a86f83f4fa87114e8936d933
Yeshin Lee <yeslee5004@gmail.com>
2021-01-05 22:03:44 +0900

    Modify ft_type

--- ft_printf/ft_parsing.c
+++ ft_printf/ft_parsing.c
@@ -6,36 +6,48 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 21:38:00 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/04 10:47:59 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/05 19:17:28 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-char	ft_type(char c)
+int			ft_type(const char *str, t_list *lst)
 {
-	if (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' ||
-			c == 'u' || c == 'x' || c == 'X' || c == '%')
-		return (c);
-	return ('0');
+	if (*str == 'c' || *str == 's' || *str == 'p' || *str == 'd' ||
+		*str == 'i' || *str == 'u' || *str == 'x' || *str == 'X' || *str == '%')
+	{
+		lst->type = *str;
+		return (0);
+	}
+	return (1);
+}
+
+void		ft_flag(const char *str, t_list *lst)
+{
+	if (*str == ' ')
+		lst->space = 1;
+	else if (*str == '+')
+		lst->plus = 1;
+	else if (*str == '-')
+		lst->left = 1;
+	else if (*str == '0')
+		lst->zero = 1;
 }
 
 const char	*ft_parsing(const char *str, t_list *lst)
 {
-	if (ft_type(*str) == '0')
+	if (ft_type(str, lst))
 	{
-		if (*str == '-')
-			lst->left = 1;
-		else if (*str == '0')
-			lst->zero = 1;
-		while (*str == '-' || *str == '0')
-    		str++;
-		//printf("str: %c\n", *str);
+		while (*str == ' ' || *str == '+' || *str == '-' || *str == '0')
+		{
+			ft_flag(str, lst);
+			str++;
+		}
 		while (ft_atoi((char *)str))
 		{
 			lst->width = (lst->width * 10) + (*str - '0');
-			//printf("width: %d\n", lst->width);
 			str++;
 		}
 		if (*str == '.')
@@ -48,7 +60,7 @@ const char	*ft_parsing(const char *str, t_list *lst)
 			}
 		}
 	}
-	if (ft_type(*str))
-		lst->type = *str;
+	else
+		ft_type(str, lst);
 	return (str);
 }
