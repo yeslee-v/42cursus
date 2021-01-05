c42b64c1990ccb14301e29cf06c222fd8c8c15e5
Yeshin Lee <yeslee5004@gmail.com>
2021-01-05 22:01:18 +0900

    Move ft_flag_d t o ft_print_d

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 22:12:53 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/04 12:00:25 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/05 20:16:54 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ typedef struct	s_list
 	int			prec;
 
 	//	char	sharp;
+	int			len_mod;
 	char		type;
 	int			len;
 	int			result;
@@ -36,9 +37,8 @@ typedef struct	s_list
 
 int				ft_printf(const char *str, ...);
 const char		*ft_parsing(const char *str, t_list *lst);
-char			ft_type(char c);
 int				ft_atoi(char *str);
-void			ft_flag_d(t_list *lst);
+void			ft_print_d(t_list *lst);
 char			*ft_itoa(int n);
 int				ft_len(int n);
 #endif
