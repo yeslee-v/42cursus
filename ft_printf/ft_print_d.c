bcdc7b16841c116fedfe72069a5a0dee6e2a9ddf
Yeshin Lee <yeslee5004@gmail.com>
2021-01-06 16:05:56 +0900

    Finish ' ''0' flag & width

--- ft_printf/ft_print_d.c
+++ ft_printf/ft_print_d.c
@@ -6,51 +6,46 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 19:43:34 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/05 22:52:45 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/06 16:00:55 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void	ft_write_res(t_list *lst)
+{
+	write(1, ft_itoa(lst->result), lst->len);
+}
+
+void	ft_width_len(t_list *lst)
+{
+	if (lst->width > lst->len)
+	{
+		if ((lst->left) || (lst->left && lst->zero))
+			ft_write_res(lst);
+		while (lst->width > lst->len)
+		{
+			if (!(lst->left) && lst->zero)
+			{
+				if (lst->result < 0)
+				{
+					write(1, "-", 1);
+					lst->result *= -1;
+				}
+				write(1, "0", 1);
+			}
+			else
+				write(1, " ", 1);
+			lst->width--;
+		}
+		if (!((lst->left) || (lst->left && lst->zero)))
+			ft_write_res(lst);
+	}
+}
+
 void	ft_print_d(t_list *lst)
 {
-	printf("len:%d\n", lst->len);
 	if (lst->result < 0)
 		lst->len++;
-	if (lst->left)
-	{
-		if (lst->width > lst->len)
-		{
-			write(1, ft_itoa(lst->result), lst->len);
-			while (lst->width > lst->len)
-			{
-				write(1, " ", 1);
-				lst->width--;
-			}
-		}
-	}
-	else if (lst->zero)
-	{
-		if (lst->width > lst->len)
-		{
-			while (lst->width > lst->len)
-			{
-				write(1, "0", 1);
-				lst->width--;
-			}
-			write(1, ft_itoa(lst->result), lst->len);
-		}
-	}
-	else if (lst->width)
-	{
-		if (lst->width > lst->len)
-		{
-			while (lst->width > lst->len)
-			{
-				write(1, " ", 1);
-				lst->width--;
-			}
-			write(1, ft_itoa(lst->result), lst->len);
-		}
-	}
+	ft_width_len(lst);
 }
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 15:03:51 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/05 22:53:45 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/06 15:59:38 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,49 @@ int main()
 {
     printf("Result of applying conversion to 123\n");
     
-	printf(  "%8d\n", 123);
-    ft_printf(  "%8d\n\n", 123);
+	printf(  "%8d|\n", 123);
+    ft_printf(  "%8d|\n\n", 123);
 	
-	printf( "%-8d\n", 123);
-	ft_printf( "%-8d\n\n", 123);
+	printf( "%-8d|\n", 123);
+	ft_printf( "%-8d|\n\n", 123);
 
 //	printf( "%+8d\n", 123);
 //	ft_printf( "%+8d\n\n", 123);
 
-//    printf( "% 8d\n", 123);
-//    ft_printf( "% 8d\n\n", 123);
+//  printf( "% 8d\n", 123);
+//  ft_printf( "% 8d\n\n", 123);
     
-    printf( "%08d\n", 123);
-    ft_printf( "%08d\n\n", 123);
-    
+	printf( "%08d|\n", 123);
+	ft_printf( "%08d|\n\n", 123);
+
+//	printf(  "%-08d\n, 123");	// 0 flag ignored == %-8d
+	ft_printf(  "%-08d|\n\n", 123);
 //	printf("%-+8d\n", 123);
-//    printf("%- 8d\n", 123);
-//    printf("%+08d\n", 123);
-//    printf("% 08d\n", 123);
+//  printf("%- 8d\n", 123);
+//  printf("%+08d\n", 123);
+//  printf("% 08d\n", 123);
 
     printf("Result of applying conversion to -123\n");
 
-	printf(  "%8d\n", -123);
-	ft_printf(  "ft:%8d\n", -123);
+	printf(  "%8d|\n", -123);
+	ft_printf(  "%8d|\n\n", -123);
 
-/*	printf( "%-8d\n", -123);
-    printf( "%+8d\n", -123);
-    printf( "% 8d\n", -123);
-    printf( "%08d\n", -123);
-    printf("%-+8d\n", -123);
-    printf("%- 8d\n", -123);
-    printf("%+08d\n", -123);
-    printf("% 08d\n", -123);
-*/
+	printf( "%-8d|\n", -123);
+	ft_printf( "%-8d|\n\n", -123);
+
+//	printf( "%+8d\n", -123);
+//  printf( "% 8d\n", -123);
+    
+	printf( "%08d|\n", -123);
+    ft_printf( "%08d|\n\n", -123);
+    
+//	printf("%-+8d\n", -123);
+//  printf("%- 8d\n", -123);
+//  printf("%+08d\n", -123);
+//  printf("% 08d\n", -123);
+	
+//	printf("%-08d|\n", -123);
+	ft_printf("%-08d|\n\n", -123);
+
 	return (0);
 }
