85e0e7096f85d65dafad151f05d112647c10cae0
Yeshin Lee <yeslee5004@gmail.com>
2021-01-23 23:51:36 +0900

    Edit function name

--- ft_printf/print_str.c
+++ ft_printf/print_str.c
@@ -6,14 +6,14 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 21:07:46 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/23 00:48:07 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/23 23:50:15 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-static void	ft_res_print(t_lst *lst)
+void	ft_res_print_s(t_lst *lst)
 {
 	if (lst->res_s == NULL)
 	{
@@ -21,27 +21,32 @@ static void	ft_res_print(t_lst *lst)
 		if (lst->prec == -1)
 			ft_putstr(lst->res_s);
 	}
-	(lst->prec && (lst->prec < lst->len)) ? ft_putnstr(lst->prec, lst->res_s)
+	else if (lst->prec == -1)
+	{
+		ft_putstr(lst->res_s);
+		return ;
+	}
+	((lst->prec) && (lst->prec < lst->len)) ? ft_putnstr(lst->prec, lst->res_s)
 											: ft_putstr(lst->res_s);
 }
 
-static void	ft_flag_off(t_lst *lst)
+void	ft_sflag_off(t_lst *lst)
 {
 	ft_flag_print(lst->left_size, ' ');
-	ft_res_print(lst);
+	ft_res_print_s(lst);
 }
 
-static void	ft_flag_on(t_lst *lst)
+void	ft_sflag_on(t_lst *lst)
 {
-	ft_res_print(lst);
+	ft_res_print_s(lst);
 	ft_flag_print(lst->left_size, ' ');
 }
 
-static void	ft_set_size(t_lst *lst)
+void	ft_set_size(t_lst *lst)
 {
 	if (lst->len < lst->width)
 	{
-		if (!(lst->dot))
+		if (!(lst->dot) || (lst->prec == -1))
 			lst->left_size = lst->width - lst->len;
 		else if (lst->prec <= lst->width)
 			lst->left_size = ((lst->len <= lst->prec) ?
@@ -64,12 +69,12 @@ void		ft_print_str(t_lst *lst)
 	}
 	else if (((lst->width <= lst->len) && !(lst->prec)))
 	{
-		ft_res_print(lst);
+		ft_res_print_s(lst);
 		lst->cnt += lst->len;
 		return ;
 	}
 	ft_set_size(lst);
 	lst->cnt += (lst->len <= lst->prec) ? lst->left_size + lst->len
 										: lst->left_size + lst->prec;
-	lst->left ? ft_flag_on(lst) : ft_flag_off(lst);
+	lst->left ? ft_sflag_on(lst) : ft_sflag_off(lst);
 }
