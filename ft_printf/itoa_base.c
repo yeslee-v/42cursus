41594e9ff59fd93a9f95b7ed95e4f93e5818e3ca
Yeshin Lee <yeslee5004@gmail.com>
2021-01-25 11:54:58 +0900

    Delete ft_max_value

--- ft_printf/itoa_base.c
+++ ft_printf/itoa_base.c
@@ -6,23 +6,11 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 16:22:07 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/23 23:13:20 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/25 11:37:14 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-char	*ft_max_value(char type)
-{
-	if (type == 'u')
-		return ("4294967295");
-	else if (type == 'X')
-		return ("FFFFFFFF");
-	else if (type == 'x')
-		return ("ffffffff");
-	else
-		return ("-1");
-}
 
 int		ft_len_base(long long num, int base, int i)
 {
@@ -44,8 +32,6 @@ char	*ft_itoa_base(long long value, int base, char type)
 	num = (value < 0) ? -value : value;
 	sign = (value < 0 && base == 10) ? 1 : 0;
 	i = (sign) ? 1 : 0;
-	if (value == 4294967295)
-		return (ft_max_value(type));
 	i = ft_len_base(num, base, i);
 	if (!(str = (char *)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
