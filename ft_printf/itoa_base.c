21410f9cf57f0eb1a7d028cf2cca6bda4ffb26ef
Yeshin Lee <yeslee5004@gmail.com>
2021-01-28 22:01:20 +0900

    Change input type

--- ft_printf/itoa_base.c
+++ ft_printf/itoa_base.c
@@ -6,13 +6,13 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 16:22:07 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/25 11:37:14 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/28 22:00:44 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_len_base(long long num, int base, int i)
+int		ft_len_base(unsigned long long num, int base, int i)
 {
 	while (num)
 	{
@@ -22,29 +22,32 @@ int		ft_len_base(long long num, int base, int i)
 	return (i);
 }
 
-char	*ft_itoa_base(long long value, int base, char type)
+char	*ft_itoa_base(unsigned long long value, int base, char type)
 {
 	int			i;
-	int			sign;
-	long long	num;
+	unsigned long long	num;
 	char		*str;
 
-	num = (value < 0) ? -value : value;
-	sign = (value < 0 && base == 10) ? 1 : 0;
-	i = (sign) ? 1 : 0;
+	num = value;
+	i = 0;
 	i = ft_len_base(num, base, i);
 	if (!(str = (char *)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
-	str[i] = (value == 0) ? '0' : '\0';
-	while (i-- + sign)
+	if (!(value))
+	{
+		str[0] = '0';
+		str[1] = '\0';
+	}
+	else
+		str[i] = '\0';
+	while (i)
 	{
 		if (num % base < 10)
-			str[i] = (num % base + '0');
+			str[--i] = (num % base + '0');
 		else
-			str[i] = ((type == 'x') || (type == 'p')) ? (num % base + 'a' - 10)
+			str[--i] = ((type == 'x') || (type == 'p')) ? (num % base + 'a' - 10)
 										: (num % base + 'A' - 10);
 		num /= base;
 	}
-	(i == 0) ? str[i] = '-' : 0;
 	return (str);
 }
