9a31cd6c41a6071faf870d0ac77bb7f835be253c
Yeshin Lee <yeslee5004@gmail.com>
2021-01-29 01:03:35 +0900

    Edit ft_istype

--- ft_printf/parse_result.c
+++ ft_printf/parse_result.c
@@ -6,20 +6,23 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 21:53:23 by yeslee            #+#    #+#             */
-/*   Updated: 2021/01/28 19:17:34 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/01/29 01:03:02 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-void	ft_istype(const char **str, t_lst *lst)
+int		ft_istype(const char **str, t_lst *lst)
 {
 	if (**str == 'c' || **str == 's' || **str == 'p' || **str == 'd' ||
 		**str == 'i' || **str == 'u' || **str == 'x' || **str == 'X' ||
 		**str == '%')
+	{
 		lst->type = **str;
-	return ;
+		return (1);
+	}
+	return (0);
 }
 
 void	ft_negative_result(t_lst *lst)
@@ -57,8 +60,8 @@ void	ft_check_len(t_lst *lst)
 		i = lst->res;
 		lst->len = (i == -2147483648) ? 10 : ft_size(i);
 	}
-	else if (lst->type == 'p' || lst->type == 'u' ||
-			lst->type == 'x' || lst->type == 'X')
+	else if (lst->type == 'p' || lst->type == 'u' || lst->type == 'x'
+			|| lst->type == 'X')
 		ft_check_len_pux(lst);
 	else if (lst->type == '%')
 		lst->len = 1;
@@ -67,27 +70,27 @@ void	ft_check_len(t_lst *lst)
 
 void	ft_check_result(const char **str, t_lst *lst, va_list *ap)
 {
-	if (**str == 'c')
-		lst->res = va_arg(*ap, int);
-	else if (**str == 's')
-		lst->res_s = va_arg(*ap, char *);
-	else if (**str == 'p')
-		lst->res = va_arg(*ap, unsigned long long);
-	else if (**str == 'd' || **str == 'i')
+	if (ft_istype(str, lst))
 	{
-		lst->res = va_arg(*ap, int);
-		ft_negative_result(lst);
+		if (**str == 'c')
+			lst->res = va_arg(*ap, int);
+		else if (**str == 's')
+			lst->res_s = va_arg(*ap, char *);
+		else if (**str == 'p')
+			lst->res = va_arg(*ap, unsigned long long);
+		else if (**str == 'd' || **str == 'i')
+		{
+			lst->res = va_arg(*ap, int);
+			ft_negative_result(lst);
+		}
+		else if (**str == 'u')
+			lst->res = va_arg(*ap, unsigned int);
+		else if (**str == 'x')
+			lst->res = va_arg(*ap, unsigned int);
+		else if (**str == 'X')
+			lst->res = va_arg(*ap, unsigned int);
+		else if (**str == '%')
+			lst->res_pct = '%';
+		ft_check_len(lst);
 	}
-	else if (**str == 'u')
-		lst->res = va_arg(*ap, unsigned int);
-	else if (**str == 'x')
-		lst->res = va_arg(*ap, unsigned int);
-	else if (**str == 'X')
-		lst->res = va_arg(*ap, unsigned int);
-	else if (**str == '%')
-		lst->res_pct = '%';
-	else
-		return ;
-	ft_istype(str, lst);
-	ft_check_len(lst);
 }
