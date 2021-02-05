32a59ac2559acf7f461a454bca8026fe05b345d5
Yeshin Lee <yeslee5004@gmail.com>
2021-02-05 20:50:36 +0900

    Initial complete

--- ft_server/srcs/run.sh
+++ ft_server/srcs/run.sh
@@ -1,7 +1,3 @@
-chmod 755 /run.sh
-chown -R www-data:www-data /var/www/
-chmod -R 755 /var/www/
-
 mv localhost.dev.crt etc/ssl/certs/
 mv localhost.dev.key etc/ssl/private/
 mv /tmp/default /etc/nginx/sites-available/default
@@ -11,21 +7,19 @@ mv phpMyAdmin-5.0.2-all-languages phpmyadmin/
 mv phpmyadmin/ /var/www/html/
 mv /tmp/config.inc.php /var/www/html/phpmyadmin/
 
-#mysql < /var/www/html/phpmyadmin/sql/create_tables.sql -u root --skip-password
 
 mv wordpress/ /var/www/html/
-mv /tmp/wp-config.php /var/www/html/wordpress/
+mv ./tmp/wp-config.php /var/www/html/wordpress/
 chown -R www-data:www-data /var/www/html/wordpress
 
 service mysql start
 
-echo	"CREATE DATABASE IF NOT EXISTS wordpress;" \
-		| mysql -u root --skip-password
-echo	"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '0406' WITH GRANT OPTION;" \
-		| mysql -u root --skip-password
-#echo	"FLUSH PRIVILEGES" | mysql -u root --skip-password
+mysql < /var/www/html/phpmyadmin/sql/create_tables.sql -u root --skip-password
+mysqladmin -u root -p password
 
-#service mysql restart
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%' IDENTIFIED BY '0406' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES" | mysql -u root --skip-password
 
 service nginx start
 service php7.3-fpm start
