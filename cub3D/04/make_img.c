8aa44d07eabfce5a82643b20f468b36440f0f8d2
Yeshin Lee <yeslee5004@gmail.com>
2021-02-28 17:29:59 +0900

    Draw 2 boxes

--- cub3D/04/make_img.c
+++ cub3D/04/make_img.c
@@ -4,13 +4,19 @@
 # define WIN_WIDTH	700
 # define WIN_HEIGHT	500
 
-# define IMG_WIDTH	200
+# define IMG_WIDTH	400
 # define IMG_HEIGHT	300
+
+# define ROWS		11
+# define COLS		15
 
 typedef struct	s_img
 {
+	void	*img; // Add at 06
+
 	void	*img_ptr;
 	int		*data;
+	
 	int		size_l;
 	int		bpp;
 	int		endian;
@@ -22,19 +28,26 @@ typedef struct	s_mlx
 	void	*win;
 }				t_mlx;
 
-void draw_box(t_img img)
+typedef	struct	s_game
+{
+	t_img	img;
+
+	int		map[ROWS][COLS];
+}				t_game;
+
+void draw_box(t_img img, int width, int height, int color)
 {
 	int		count_w;
 	int		count_h;
 
-	count_h = 200;
-	while (++count_h < IMG_HEIGHT)
+	count_h = height;
+	while (++count_h < IMG_HEIGHT) // 400
 	{
-		count_w = -1;
-		while (++count_w < IMG_WIDTH)
+		count_w = width;
+		while (++count_w < IMG_WIDTH) // 300
 		{
-			if (count_w % 2)
-				img.data[count_h * IMG_WIDTH + count_w] = 0x890aff;
+			if (count_w)
+				img.data[count_h * IMG_WIDTH + count_w] = color;
 			else
 				img.data[count_h * IMG_WIDTH + count_w] = 0x000000;
 		}
@@ -51,7 +64,8 @@ int	main(void)
 	img.img_ptr = mlx_new_image(mlx->mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
 	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bpp, &img.size_l, &img.endian);
 	
-	draw_box(img);
+	draw_box(img, -1, 200, 0x0a80f0);
+	draw_box(img, 250, 100, 0x890aff);
 	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win, img.img_ptr, 0, 0);
 	mlx_loop(mlx->mlx_ptr);
 	return (0);
