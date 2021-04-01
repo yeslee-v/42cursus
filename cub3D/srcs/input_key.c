240cd02dd1791bd0351bd8cd0b93848b94404b4a
Yeshin Lee <yeslee5004@gmail.com>
2021-04-01 12:01:35 +0900

    Complete Norm3

--- cub3D/srcs/input_key.c
+++ cub3D/srcs/input_key.c
@@ -6,77 +6,109 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 18:38:23 by yeslee            #+#    #+#             */
-/*   Updated: 2021/03/31 18:55:02 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/04/01 12:00:50 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int ft_press_key(int key, t_all *all)
+int	ft_press_esc(int key, t_all *all)
 {
-	//map_x
-    if (key == KEY_W)
-    {
-        if (all->map.map[(int)(all->info.posX + all->info.dirX * all->info.moveSpeed)][(int)(all->info.posY)] != '1')
-            all->info.posX += all->info.dirX * all->info.moveSpeed;
-        if (all->map.map[(int)(all->info.posX)][(int)(all->info.posY + all->info.dirY * all->info.moveSpeed)] != '1')
-            all->info.posY += all->info.dirY * all->info.moveSpeed;
-    }
-
-    if (key == KEY_S)
-    {
-        if (all->map.map[(int)(all->info.posX - all->info.dirX * all->info.moveSpeed)][(int)(all->info.posY)] != '1')
-            all->info.posX -= all->info.dirX * all->info.moveSpeed;
-        if (all->map.map[(int)(all->info.posX)][(int)(all->info.posY - all->info.dirY * all->info.moveSpeed)] != '1')
-            all->info.posY -= all->info.dirY * all->info.moveSpeed;
-    }
-
-    if (key == KEY_D)
-    {
-        if (all->map.map[(int)(all->info.posX + all->info.dirY * all->info.moveSpeed)][(int)(all->info.posY)] != '1')
-            all->info.posX += all->info.dirY * all->info.moveSpeed;
-        if (all->map.map[(int)(all->info.posX)][(int)(all->info.posY - all->info.dirX * all->info.moveSpeed)] != '1')
-            all->info.posY -= all->info.dirX * all->info.moveSpeed;
-    }
-
-    if (key == KEY_A)
-    {
-        if (all->map.map[(int)(all->info.posX - all->info.dirY * all->info.moveSpeed)][(int)(all->info.posY)] != '1')
-            all->info.posX -= all->info.dirY * all->info.moveSpeed;
-        if (all->map.map[(int)(all->info.posX)][(int)(all->info.posY + all->info.dirX * all->info.moveSpeed)] != '1')
-            all->info.posY += all->info.dirX * all->info.moveSpeed;
-    }
-
-    if (key == KEY_ARROW_L)
-    {
-        double oldDirectionX = all->info.dirX;
-        all->info.dirX = all->info.dirX * cos(all->info.rotSpeed) - all->info.dirY * sin(all->info.rotSpeed);
-        all->info.dirY = oldDirectionX * sin(all->info.rotSpeed) + all->info.dirY * cos(all->info.rotSpeed);
-        double oldPlaneX = all->info.planeX;
-        all->info.planeX = all->info.planeX * cos(all->info.rotSpeed) - all->info.planeY * sin(all->info.rotSpeed);
-        all->info.planeY = oldPlaneX * sin(all->info.rotSpeed) + all->info.planeY * cos(all->info.rotSpeed);
-    }
-
-    if (key == KEY_ARROW_R)
-    {
-        double oldDirectionX = all->info.dirX;
-        all->info.dirX = all->info.dirX * cos(-all->info.rotSpeed) - all->info.dirY * sin(-all->info.rotSpeed);
-        all->info.dirY = oldDirectionX * sin(-all->info.rotSpeed) + all->info.dirY * cos(-all->info.rotSpeed);
-        double oldPlaneX = all->info.planeX;
-        all->info.planeX = all->info.planeX * cos(-all->info.rotSpeed) - all->info.planeY * sin(-all->info.rotSpeed);
-        all->info.planeY = oldPlaneX * sin(-all->info.rotSpeed) + all->info.planeY * cos(-all->info.rotSpeed);
-    }
-    if (key == KEY_ESC)
+	if (key == KEY_ESC)
 	{
 		ft_bgm_end();
-        exit(0);
+		exit(0);
 	}
-    return (0);
+	return (0);
 }
 
-int		ft_close(int key, t_all *all)
+int	ft_press_arrow(int key, t_all *all, double old_dirx, double old_planex)
 {
-	ft_bgm_end();
-	exit(0);
+	if (key == KEY_ARROW_L)
+	{
+		all->info.dirX = all->info.dirX * cos(all->info.rotSpeed) -
+			all->info.dirY * sin(all->info.rotSpeed);
+		all->info.dirY = old_dirx * sin(all->info.rotSpeed) +
+			all->info.dirY * cos(all->info.rotSpeed);
+		all->info.planeX = all->info.planeX * cos(all->info.rotSpeed) -
+			all->info.planeY * sin(all->info.rotSpeed);
+		all->info.planeY = old_planex * sin(all->info.rotSpeed) +
+			all->info.planeY * cos(all->info.rotSpeed);
+	}
+	if (key == KEY_ARROW_R)
+	{
+		all->info.dirX = all->info.dirX * cos(-all->info.rotSpeed) -
+			all->info.dirY * sin(-all->info.rotSpeed);
+		all->info.dirY = old_dirx * sin(-all->info.rotSpeed) +
+			all->info.dirY * cos(-all->info.rotSpeed);
+		all->info.planeX = all->info.planeX * cos(-all->info.rotSpeed) -
+			all->info.planeY * sin(-all->info.rotSpeed);
+		all->info.planeY = old_planex * sin(-all->info.rotSpeed) +
+			all->info.planeY * cos(-all->info.rotSpeed);
+	}
+	return (0);
+}
+
+int	ft_press_key_ad(int key, t_all *all, double vec_x, double vec_y)
+{
+	if (key == KEY_A)
+	{
+		if (all->map.map[(int)(all->info.posX - vec_y)]
+				[(int)(all->info.posY)] != '1')
+			all->info.posX -= vec_y;
+		if (all->map.map[(int)(all->info.posX)]
+				[(int)(all->info.posY + vec_x)] != '1')
+			all->info.posY += vec_x;
+	}
+	if (key == KEY_D)
+	{
+		if (all->map.map[(int)(all->info.posX + vec_y)]
+				[(int)(all->info.posY)] != '1')
+			all->info.posX += vec_y;
+		if (all->map.map[(int)(all->info.posX)]
+				[(int)(all->info.posY - vec_x)] != '1')
+			all->info.posY -= vec_x;
+	}
+	return (0);
+}
+
+int	ft_press_key_ws(int key, t_all *all, double vec_x, double vec_y)
+{
+	if (key == KEY_W)
+	{
+		if (all->map.map[(int)(all->info.posX + vec_x)]
+				[(int)(all->info.posY)] != '1')
+			all->info.posX += vec_x;
+		if (all->map.map[(int)(all->info.posX)]
+				[(int)(all->info.posY + vec_y)] != '1')
+			all->info.posY += vec_y;
+	}
+	if (key == KEY_S)
+	{
+		if (all->map.map[(int)(all->info.posX - vec_x)]
+				[(int)(all->info.posY)] != '1')
+			all->info.posX -= vec_x;
+		if (all->map.map[(int)(all->info.posX)]
+				[(int)(all->info.posY - vec_y)] != '1')
+			all->info.posY -= vec_y;
+	}
+	return (0);
+}
+
+int	ft_press_key(int key, t_all *all)
+{
+	double	vec_x;
+	double	vec_y;
+	double	old_dirx;
+	double	old_planex;
+
+	old_dirx = all->info.dirX;
+	old_planex = all->info.planeX;
+	vec_x = all->info.dirX * all->info.moveSpeed;
+	vec_y = all->info.dirY * all->info.moveSpeed;
+	ft_press_key_ws(key, all, vec_x, vec_y);
+	ft_press_key_ad(key, all, vec_x, vec_y);
+	ft_press_arrow(key, all, old_dirx, old_planex);
+	ft_press_esc(key, all);
 	return (0);
 }
