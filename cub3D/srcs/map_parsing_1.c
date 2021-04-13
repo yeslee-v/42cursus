68c4bec0663b3e11d283cf0384d9583c0e82df5b
Yeshin Lee <yeslee5004@gmail.com>
2021-04-13 15:13:51 +0900

    Add invalid cases

--- cub3D/srcs/map_parsing_1.c
+++ cub3D/srcs/map_parsing_1.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 18:53:02 by yeslee            #+#    #+#             */
-/*   Updated: 2021/04/13 11:50:03 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/04/13 15:11:21 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,27 @@ void	ft_read_line(char *line, t_all *all)
 {
 	if (all->game.cnt == 8)
 		ft_read_map(line, all);
-	else if (ft_isspace(line[0]))
-		ft_error_message(5);
-	if ((line[0] == 'R') && (ft_isspace(line[1])))
-		ft_put_size((line + 2), &(all->game));
-	else if ((line[0] == 'N') && (line[1] == 'O') && (ft_isspace(line[2])))
-		ft_put_image((line + 3), &(all->game.n), &(all->game));
-	else if ((line[0] == 'S') && (line[1] == 'O') && (ft_isspace(line[2])))
-		ft_put_image((line + 3), &(all->game.s), &(all->game));
-	else if ((line[0] == 'W') && (line[1] == 'E') && (ft_isspace(line[2])))
-		ft_put_image((line + 3), &(all->game.e), &(all->game));
-	else if ((line[0] == 'E') && (line[1] == 'A') && (ft_isspace(line[2])))
-		ft_put_image((line + 3), &(all->game.w), &(all->game));
-	else if ((line[0] == 'S') && (ft_isspace(line[1])))
-		ft_put_image((line + 2), &(all->game.sp), &(all->game));
-	else if ((line[0] == 'F') && (ft_isspace(line[1])))
-		ft_put_color((line + 2), all->game.f, &(all->game));
-	else if ((line[0] == 'C') && (ft_isspace(line[1])))
-		ft_put_color((line + 2), all->game.c, &(all->game));
 	else
-		printf("res:%s\n", line);
+	{
+		if (ft_isspace(line[0]))
+			ft_error_message(5);
+		if ((line[0] == 'R') && (ft_isspace(line[1])))
+			ft_put_size((line + 2), &(all->game));
+		else if ((line[0] == 'N') && (line[1] == 'O') && (ft_isspace(line[2])))
+			ft_put_image((line + 3), &(all->game.n), &(all->game));
+		else if ((line[0] == 'S') && (line[1] == 'O') && (ft_isspace(line[2])))
+			ft_put_image((line + 3), &(all->game.s), &(all->game));
+		else if ((line[0] == 'W') && (line[1] == 'E') && (ft_isspace(line[2])))
+			ft_put_image((line + 3), &(all->game.e), &(all->game));
+		else if ((line[0] == 'E') && (line[1] == 'A') && (ft_isspace(line[2])))
+			ft_put_image((line + 3), &(all->game.w), &(all->game));
+		else if ((line[0] == 'S') && (ft_isspace(line[1])))
+			ft_put_image((line + 2), &(all->game.sp), &(all->game));
+		else if ((line[0] == 'F') && (ft_isspace(line[1])))
+			ft_put_color((line + 2), all->game.f, &(all->game));
+		else if ((line[0] == 'C') && (ft_isspace(line[1])))
+			ft_put_color((line + 2), all->game.c, &(all->game));
+		else
+			ft_error_message(6);
+	}
 }
