ac85c0689c4585f2f05610b2d4793353365cf0c0
Yeshin Lee <yeslee5004@gmail.com>
2021-04-28 00:24:08 +0900

    Change tail to head

--- push_swap/srcs/s_series.c
+++ push_swap/srcs/s_series.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 12:42:17 by yeslee            #+#    #+#             */
-/*   Updated: 2021/04/27 17:26:25 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/04/28 00:23:34 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ void	ft_sa(t_lst *lst)
 {
 	int	tmp;
 
-	tmp = lst->tail->data;
-	lst->tail->data = lst->tail->prev->data;
-	lst->tail->prev->data = tmp;
+	tmp = lst->head->data;
+	lst->head->data = lst->head->next->data;
+	lst->head->next->data = tmp;
 }
 
 void	ft_sb(t_lst *lst)
 {
 	int	tmp;
 
-	tmp = lst->tail->data;
-	lst->tail->data = lst->tail->prev->data;
-	lst->tail->prev->data = tmp;
+	tmp = lst->head->data;
+	lst->head->data = lst->head->next->data;
+	lst->head->next->data = tmp;
 }
