6b5ba399a5c16847fdcd9255de5b02c5128dadf3
yeslee-v <yeslee5004@gmail.com>
2021-05-05 13:29:38 +0900

    Initial complete checker

--- push_swap/srcs/checker.c
+++ push_swap/srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: yeslee <yeslee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 15:17:20 by yeslee            #+#    #+#             */
-/*   Updated: 2021/05/04 23:56:15 by yeslee           ###   ########.fr       */
+/*   Updated: 2021/05/05 13:28:16 by yeslee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,17 @@
 int		ft_is_sorted(t_lst *stack)
 {
 	int		i;
-	int		j;
 	t_node	*first;
 	t_node	*second;
 
-	i = stack->cnt + 1;
+	i = stack->cnt;
 	first = stack->head;
+	second = first->next;
 	while (--i)
 	{
-		j = i;
-		second = first->next;
-		while (--j)
-		{
-			if (second->data < first->data)
-				return (0);
-			second = second->next;
-		}
+		if (second->data < first->data)
+			return (0);
+		second = second->next;
 		first = first->next;
 	}
 	return (1);
@@ -50,6 +45,8 @@ void	ft_r_first(t_lst *a, t_lst *b, char *line)
 		ft_rrb(b);
 	else if ((line[1] == 'r') && (line[2] == 'r') && (line[3] == '\0'))
 		ft_rrr(a, b);
+	else
+		ft_error_message();
 }
 
 void	ft_read(t_lst *a, t_lst *b)
@@ -71,8 +68,8 @@ void	ft_read(t_lst *a, t_lst *b)
 			ft_pb(b, a);
 		else if (line[0] == 'r')
 			ft_r_first(a, b, line);
-		write(1, line, ft_strlen(line));
-		write(1, "\n", 1);
+		else
+			ft_error_message();
 		free(line);
 	}
 	if (size == -1)
