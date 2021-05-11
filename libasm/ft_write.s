6be50374a6b96925e679e180e0b6cf0ba5410962
Yeshin Lee <yeslee5004@gmail.com>
2021-05-11 23:41:37 +0900

    Initial finish

---
+++ libasm/ft_write.s
@@ -0,0 +1,19 @@
+section .text
+		global _main
+
+extern __error
+
+_main:
+	move rax, 0x2000004
+	syscall
+	jc _err
+	ret
+
+_err:
+	push rax
+	call ___error
+	pop rdx
+	mov [rax], rdx
+	mov rax, -1
+	ret
+
