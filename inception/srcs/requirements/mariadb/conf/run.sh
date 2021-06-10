2347e764f953ec2102bf12a5a7f770782a8fd9c7
yeslee-v <yeslee5004@gmail.com>
2021-06-10 22:50:43 +0900

    Add volume and network

--- inception/srcs/requirements/mariadb/conf/run.sh
+++ inception/srcs/requirements/mariadb/conf/run.sh
@@ -6,3 +6,6 @@ mysqladmin -u root -p password
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root
 echo "GRANT ALL PRIVILAGES ON wordpress.* TO 'yeslee'@'%' IDENTIFIED BY '0406' WITH GRANT OPTION;" | mysql -u root
 echo "FLUSH PRIVILAGES" | mysql -u root
+
+# run in background
+mysqld_safe
--- inception/srcs/requirements/wordpress/conf/run.sh
+++ inception/srcs/requirements/wordpress/conf/run.sh
@@ -1,3 +1,6 @@
 mv wordpress/ /var/www/html/
 mv ./tmp/wp-config.php /var/www/html/wordpress/
 chown -R www-data:www-data /var/www/html/wordpress
+
+# run in background
+php-fpm7.3 -F -R
