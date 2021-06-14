5b1a5dcf0730ab2d04be99be157791d3c91296fa
yeslee-v <yeslee5004@gmail.com>
2021-06-14 22:40:24 +0900

    Try to add env in mysql

--- inception/srcs/requirements/mariadb/conf/run.sh
+++ inception/srcs/requirements/mariadb/conf/run.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-service mysql start
 
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root
-echo "CREATE USER IF NOT EXISTS 'yeslee'@'%' IDENTIFIED BY '1234';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'yeslee'@'%' IDENTIFIED BY '1234' WITH GRANT OPTION;" | mysql -u root
-echo "FLUSH PRIVILEGES" | mysql -u root
+./mysql_cmd.sh
+
+service mysql start
+mysql -u root -p$MY_ROOT_PASSWORD < mysql_cmd.sql
 
 sleep 2
 
@@ -13,7 +12,10 @@ sleep 2
 #if [[ $(ps -aux | grep mysqld | wc -l) -gt 0 ]]; then
 #	killall -9 mysqld mysqld_safe
 #fi
+
 kill -9 `ps -ef | grep mysql | awk '{print $2}'`
+
 sleep 2
+
 # run in background
 /usr/bin/mysqld_safe --user=root --datadir=/var/lib/mysql/ --general-log-file=/dev/stderr
