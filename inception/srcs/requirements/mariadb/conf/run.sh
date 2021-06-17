68052f25f7b84cb004e20d0fccc8519f2c417d1a
yeslee-v <yeslee5004@gmail.com>
2021-06-17 16:48:49 +0900

    Complete Inception

--- inception/srcs/requirements/mariadb/conf/run.sh
+++ inception/srcs/requirements/mariadb/conf/run.sh
@@ -4,15 +4,6 @@ bash mysql_cmd.sh
 
 service mysql start
 mysql -u root -p$MYSQL_ROOT_PASSWORD < mysql_cmd.sql
-mysql -u root -p$MYSQL_ROOT_PASSWORD < wp.sql
-
-#sleep 2
-
-#killall mysqld mysqld_safe
-
-#if [[ $(ps -aux | grep mysqld | wc -l) -gt 0 ]]; then
-#	killall -9 mysqld mysqld_safe
-#fi
 
 kill -9 `ps -ef | grep mysqld | awk '{print $2}'`
 
--- inception/srcs/requirements/wordpress/conf/run.sh
+++ inception/srcs/requirements/wordpress/conf/run.sh
@@ -3,31 +3,27 @@ cat << EOF > exit.sql
 exit
 EOF
 
-chown -R www-data:www-data /var/www
+connect_db()
+{
+	while ! mysql -hmariadb -P3306 -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD < exit.sql ; do
+		if [ $status -ge 30 ]; then
+			printf "FAILED connect to db\n"
+			exit 1
+		fi
+		printf "Trying to connect to db ($status/30)\n"
+		status=$((status+1))
+		sleep 1
+	done
+	echo -e "Success."
+}
 
-#service php7.3-fpm start
-#service php7.3-fpm stop
+connect_db
+echo "error_log = /dev/stderr" >> /etc/php/7.3/fpm/php.ini
 
-#connect_db()
-#{
-#	while ! mysql -hmariadb -P3306 -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD < exit.sql ; do
-#		if [ $status -ge 30 ]; then
-#			printf "FAILED connect to db\n"
-#			exit 1
-#		fi
-#		printf "Trying to connect to db ($status/30)\n"
-#		status=$((status+1))
-#		sleep 1
-#	done
-#	echo -e "Success."
-#}
-#
-#connect_db
-#echo "error_log = /dev/stderr" >> /etc/php/7.3/fpm/php.ini
-
-#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-#chmod +x wp-cli.phar
-#php wp-cli.phar core install --url=localhost --title=yesleenception_world --admin_user=yeslee --admin_email=1234@42.fr --allow-root
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core install --path=/var/www/html/wordpress --admin_password=1234 --url=https://yeslee.42.fr --title=highway_to_hell --admin_user=yeslee --admin_email=1234@42.fr --allow-root --skip-email
 
 # run in background
 /usr/sbin/php-fpm7.3 -F
