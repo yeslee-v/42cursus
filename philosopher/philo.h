b061d21f3d5b30dad824934719e320fb15a08e30
Yeshin Lee <yeslee5004@gmail.com>
2021-08-14 23:27:44 +0900

    [Add] ft_atoi.c

--- philosopher/philo.h
+++ philosopher/philo.h
@@ -1,11 +1,18 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <unistd.h>
+# include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <pthread.h>
-
+# include <string.h>
 # include <sys/time.h>
+# include <unistd.h>
 
-#endif
+typedef struct	s_philo
+{
+	int			num_of_philo;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+}				t_philo;
+# endif
