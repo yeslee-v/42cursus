a34b9f2bdb13676b6e905ece62090064b915fe54
Yeshin Lee <yeslee5004@gmail.com>
2021-11-07 00:05:19 +0900

    Done: megaphone

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cctype>
 
 int main(int ac, char **av)
 {
-	std::cout << ac << std::endl;
-	std::cout << ac << std::endl;
+	int		i;
+	int		j;
+	char	c;
+
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (i = 1; i < ac; i++)
+		{
+			for (j = 0; av[i][j]; j++)
+			{
+				c = toupper(av[i][j]);
+				std::cout << c;
+			}
+		}
+		std::cout << std::endl;
+	}
 	return 0;
 }
