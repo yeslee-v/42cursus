43300eb8fad8f7f74020425e4f0c923edf756942
Yeshin Lee <yeslee5004@gmail.com>
2021-12-15 00:43:21 +0900

    [Add] conditions

--- cpp_module_00/ex01/phonebook.cpp
+++ cpp_module_00/ex01/phonebook.cpp
@@ -1,5 +1,29 @@
-//
-// Created by Yeshin Lee on 2021/12/14.
-//
-
+#include "iostream"
 #include "phonebook.h"
+
+int main()
+{
+    std::string opt;
+
+    while (1)
+    {
+        std::cout << "Input Option: ";
+        std::cin >> opt;
+
+        if (opt == "ADD")
+        {
+            std::cout << "here is ADD" << std::endl;
+        }
+        else if (opt == "SEARCH")
+        {
+            std::cout << "here is SEARCH" << std::endl;
+        }
+        else if (opt == "EXIT")
+        {
+            std::cout << "here is EXIT" << std::endl;
+            exit(0);
+        }
+    }
+
+    return 0;
+}
