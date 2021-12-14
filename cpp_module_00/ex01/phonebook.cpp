64c56d6ba7e60b4101a17c8a636c702525b06347
Yeshin Lee <yeslee5004@gmail.com>
2021-12-14 23:57:43 +0900

    [Init] Makefile

---
+++ cpp_module_00/ex01/phonebook.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yeshin Lee on 2021/12/14.
+//
+
+#include "phonebook.h"
---
+++ cpp_module_00/ex01/phonebook.h
@@ -0,0 +1,16 @@
+//
+// Created by Yeshin Lee on 2021/12/14.
+//
+
+#ifndef INC_42CURSUS_PHONEBOOK_H
+#define INC_42CURSUS_PHONEBOOK_H
+
+
+
+class phonebook {
+
+};
+
+
+
+#endif //INC_42CURSUS_PHONEBOOK_H
