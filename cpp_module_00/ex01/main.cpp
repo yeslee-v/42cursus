f01ae4bfa90c6f56fb04a3b663cbd4c15ae22533
Yeshin Lee <yeslee5004@gmail.com>
2021-12-24 23:36:55 +0900

    [Modify] move functions to contact.cpp

--- cpp_module_00/ex01/main.cpp
+++ cpp_module_00/ex01/main.cpp
@@ -12,56 +12,29 @@
 
 #include "phonebook.hpp"
 
-void PrintOpt(void) {
-    // std::cout << idx, firstName, lastName, nickname
-    std::cout << std::setfill(' ');
-    std::cout << "|" << std::setw(10) << "index";
-    std::cout << "|" << std::setw(10) << "first name";
-    std::cout << "|" << std::setw(10) << "last name";
-    std::cout << "|" << std::setw(10) << "nickname";
-    std::cout << std::endl;
-}
 
-void PrintLine(void) {
-    std::cout << "|" << std::setfill('-') << std::setw(10);
-    std::cout << "|" << std::setw(10);
-    std::cout << "|" << std::setw(10);
-    std::cout << "|" << std::setw(10);
-    std::cout << "|" << std::setw(10);
-    std::cout << "|" << std::endl;
-}
 
-int main()
-{
-    int         idx;
-    int         cnt;
-    Phonebook   info;
+int main(void) {
+    int         idx = 1;
     std::string cmd;
+    Phonebook   info;
 
-    idx = 0;
-    cnt = 0;
     while (1)
     {
+        std::cout << std::endl;
         std::cout << "Choose Option(ADD, SEARCH, EXIT): ";
         std::getline(std::cin, cmd);
-
         if ((cmd == "ADD") && (cmd.length() == 3))
         {
-            idx = cnt % 8;
+            if (idx > 8)
+                idx = idx % 8;
             info.AddInfo(idx);
             std::cout << std::endl;
             idx++;
         }
         else if ((cmd == "SEARCH") && (cmd.length() == 6))
         {
-            // 각 열 : 너비 10 문자, 가로 정렬, ‘|’ 문자로 구분 → 열 너비보다 긴 출력 문자열은 잘리고 마지막 표시 가능 문자는 점(.)으로 치환
-            PrintLine();
-            PrintOpt();
             info.PrintInfo(idx);
-
-            // 원하는 연락처 항목의 인덱스 입력 → 한 필드당 한 줄씩 출력
-            // 의미없는 입력값에 대해 적절한 동작 정의 + 그 외의 입력값은 버려짐
-//            std::cout << "here is SEARCH" << std::endl;
         }
         else if ((cmd == "EXIT") && (cmd.length() == 4))
         {
@@ -70,6 +43,7 @@ int main()
         }
         else
             std::cout << "command not found: " << cmd << std::endl;
+        std::cin.clear();
     }
     return (0);
 }
