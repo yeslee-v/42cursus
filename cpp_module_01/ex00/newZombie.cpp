8c063e6d8396279be682076d6b90db5c7adaabd3
Yeshin Lee <yeslee5004@gmail.com>
2021-12-28 23:20:05 +0900

    [Update] newZombie

--- cpp_module_01/ex00/newZombie.cpp
+++ cpp_module_01/ex00/newZombie.cpp
@@ -4,6 +4,6 @@
 
 #include "Zombie.hpp"
 
-Zombie* Zombie::newZombie(std::string name) {
-    // Zombie를 생성하고, 이름짓고, 코드의 어디에선가 재사용할 수 있도록 반환하는 함수
+Zombie* newZombie(std::string name) {
+    return (new Zombie(name));
 }
