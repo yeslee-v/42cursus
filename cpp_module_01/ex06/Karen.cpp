f390ff332fd7f05bd79b85f8219d76e45d23ee4f
Yeshin Lee <yeslee5004@gmail.com>
2022-01-05 11:42:43 +0900

    [Init] Karen.cpp using switch

---
+++ cpp_module_01/ex06/Karen.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Yeshin Lee on 2022/01/05.
+//
+
+#include "Karen.hpp"
+
+Karen::Karen() {
+    list[DEBUG].level = "DEBUG";
+    list[DEBUG].p = &Karen::debug;
+
+    list[INFO].level = "INFO";
+    list[INFO].p = &Karen::info;
+
+    list[WARNING].level = "WARNING";
+    list[WARNING].p = &Karen::warning;
+
+    list[ERROR].level = "ERROR";
+    list[ERROR].p = &Karen::error;
+}
+
+Karen::~Karen() {}
+
+void Karen::debug(void) {
+    std::cout << "[ DEBUG ]" << std::endl << "I love to get this!" << std::endl;
+}
+
+void Karen::info(void) {
+    std::cout << "[ INFO ]" << std::endl << "If you did I would not have to ask for it!" << std::endl;
+}
+
+void Karen::warning(void) {
+    std::cout << "[ WARNING ]" << std::endl << "This is a WARNING." << std::endl;
+}
+
+void Karen::error(void) {
+    std::cout << "[ ERROR ]" << std::endl << "I want to speak to the manager now." << std::endl;
+}
+
+void    Karen::filter(std::string level) {
+    int i;
+
+    for (i = 0; i < 4; i++)
+        if (list[i].level == level)
+            break;
+
+    switch (i) {
+        case DEBUG:
+            (this->*list[DEBUG].p)();
+            std::cout << std::endl;
+        case INFO:
+            (this->*list[INFO].p)();
+            std::cout << std::endl;
+        case WARNING:
+            (this->*list[WARNING].p)();
+            std::cout << std::endl;
+        case ERROR:
+            (this->*list[ERROR].p)();
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+            break;
+    }
+}
