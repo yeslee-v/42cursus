24650ce7fc7ea6f563880b79717f3c2bcd151b31
Yeshin Lee <yeslee5004@gmail.com>
2022-01-06 23:57:48 +0900

    [Add] OCCF functions

--- cpp_module_02/ex00/Fixed.cpp
+++ cpp_module_02/ex00/Fixed.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Fixed.hpp"
+
+Fixed::Fixed() {
+    this->raw = 0;
+    this->bits = 8;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& origin) {
+    this->raw = origin.raw;
+    this->bits = 8;
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& fixed) {
+    std::cout << "Assignation operator called" << std::endl;
+    std::cout << "getRawBits member function called" << std::endl;
+    this->raw = fixed.raw;
+    this->bits = 8;
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->raw);
+}
+
+void Fixed::setRawBits(int const raw) {
+    this->raw = raw;
+}
