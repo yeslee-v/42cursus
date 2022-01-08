cc011aa85861cb9fd87cbc80ce9681398704e07a
Yeshin Lee <yeslee5004@gmail.com>
2022-01-08 19:07:06 +0900

    [Init] main.cpp

---
+++ cpp_module_02/ex02/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Yeshin Lee on 2022/01/07.
+//
+
+#include "Fixed.hpp"
+
+int main( void ) {
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+
+    std::cout << b << std::endl;
+
+    std::cout << Fixed::max( a, b ) << std::endl;
+
+    return 0;
+}
