5edc4ddf535bf2be899f90a81f77f2244d14c5dc
Yeshin Lee <yeslee5004@gmail.com>
2022-01-08 19:04:39 +0900

    [Add] functions
    1. constructor for int, float
    2. setRawBits for float
    3. toInt, toFloat
    4. ostream operator<<

--- cpp_module_02/ex01/Fixed.cpp
+++ cpp_module_02/ex01/Fixed.cpp
@@ -11,6 +11,16 @@ Fixed::Fixed() {
     this->fixed_point = 0;
 }
 
+Fixed::Fixed(int raw) { // 정수 -> 고정 소수점으로
+    std::cout << "Int constructor called" << std::endl;
+    this->fixed_point = raw << Fixed::fractional_bits;
+}
+
+Fixed::Fixed(float raw) { // 부동 소수점 -> 고정 소수점으로
+    std::cout << "Float constructor called" << std::endl;
+    this->fixed_point = roundf(raw * (1 << Fixed::fractional_bits));
+}
+
 Fixed::Fixed(const Fixed& fixed) {
     std::cout << "Copy constructor called" << std::endl;
     *this = fixed;
@@ -27,10 +37,27 @@ Fixed::~Fixed() {
 }
 
 int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called" << std::endl;
     return this->fixed_point;
 }
 
 void Fixed::setRawBits(int const raw) {
     this->fixed_point = raw << Fixed::fractional_bits;
+}
+
+void Fixed::setRawBits(float const raw) {
+    this->fixed_point = raw * (1 << Fixed::fractional_bits);
+}
+
+int Fixed::toInt(void) const { // 고정 소수점 값 -> 정수 값
+    return this->fixed_point >> Fixed::fractional_bits;
+}
+
+float Fixed::toFloat(void) const {// 고정 소수점 값 -> 부동 소수점 상수
+    return (float)(this->fixed_point) / (1 << Fixed::fractional_bits);
+}
+
+// 매개변수의 출력 스트림에 고정 소수점 값의 부동 소수점 삽입
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+    os << fixed.toFloat();
+    return os;
 }
