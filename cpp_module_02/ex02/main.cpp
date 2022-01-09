9e39a9bac1607c455ef42cd3c7504050b07844d4
Yeshin Lee <yeslee5004@gmail.com>
2022-01-09 23:56:10 +0900

    [Add] examples for test

--- cpp_module_02/ex02/main.cpp
+++ cpp_module_02/ex02/main.cpp
@@ -8,15 +8,41 @@ int main( void ) {
     Fixed a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "pre-increment : " << ++a << std::endl;
+    std::cout << "post-increment : " << a++ << std::endl;
+    std::cout << "pre-decrement : " << --a << std::endl;
+    std::cout << "post-decrement : " << a-- << std::endl;
 
-    std::cout << b << std::endl;
+    std::cout << std::endl;
+    std::cout << "after a = " << a << std::endl; // 0
+    std::cout << "b = " << b << std::endl; // 10.1016
 
-    std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << std::endl;
+    std::cout << "[comparison operators]" << std::endl;
+    if (a>b)
+        std::cout << "a is bigger than b" << std::endl;
+    if (a<b)
+        std::cout << "b is bigger than a" << std::endl;
+    if (a>=b)
+        std::cout << "a is equal or bigger than b" << std::endl;
+    if (a<=b)
+        std::cout << "b is equal or bigger than a" << std::endl;
+    if (a==b)
+        std::cout << "a is equal b" << std::endl;
+    if (a!=b)
+        std::cout << "a is not equal b" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "[arithmetic operators]" << std::endl;
+    std::cout << "a + b = " << a+b << std::endl;
+    std::cout << "a - b = " << a-b << std::endl;
+    std::cout << "a * b = " << a*b << std::endl;
+    std::cout << "a / b = " << a/b << std::endl;
+
+//    std::cout << Fixed::max( a, b ) << std::endl; // 10.1016
+    std::cout << "min is " << Fixed::min( a, b ) << std::endl; // 10.1016
+    std::cout << "max is " << Fixed::max( a, b ) << std::endl; // 10.1016
 
     return 0;
 }
