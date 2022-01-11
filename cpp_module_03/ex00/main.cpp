be601810562ec3602a364a3abe806325871b2921
Yeshin Lee <yeslee5004@gmail.com>
2022-01-11 22:26:09 +0900

    [Init] main.cpp

--- cpp_module_03/ex00/main.cpp
+++ cpp_module_03/ex00/main.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "ClapTrap.hpp"
+
+int main(void) {
+    std::string const cula;
+
+    ClapTrap zizi = ClapTrap("zizi");
+    ClapTrap kongkong = ClapTrap("kongkong");
+    ClapTrap kebi = ClapTrap("kebi");
+
+    std::cout << std::endl;
+    zizi.attack(kebi.getName());
+    kebi.takeDamage(zizi.getHitPoints());
+
+    std::cout << std::endl;
+    kebi.beRepaired(5);
+    kongkong.attack(kebi.getName());
+    zizi.takeDamage(kongkong.getHitPoints());
+    kebi.beRepaired(3);
+    std::cout << std::endl;
+}
