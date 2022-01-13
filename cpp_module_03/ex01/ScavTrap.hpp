9227d5e2ca95844d700dc582966658cb0a3b0433
Yeshin Lee <yeslee5004@gmail.com>
2022-01-14 00:45:14 +0900

    [Modify] inheritance members

--- cpp_module_03/ex01/ScavTrap.hpp
+++ cpp_module_03/ex01/ScavTrap.hpp
@@ -5,17 +5,19 @@
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP
 
-#include <iostream>
 #include "ClapTrap.hpp"
 
-class ScavTrap : ClapTrap {
-private:
+class ScavTrap: public ClapTrap {
 public:
     ScavTrap();
+    ScavTrap(std::string name);
     ScavTrap(const ScavTrap& scavTrap);
-    ScavTrap& operator(const ScavTrap& scavTrap);
+    ScavTrap& operator=(const ScavTrap& scavTrap);
     ~ScavTrap();
 
+    void attack(std::string const& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
     void guardGate(void);
 };
 
