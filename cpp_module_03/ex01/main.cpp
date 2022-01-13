d825f4d85eb1dd7aefca384e8e7aa943d7772f83
Yeshin Lee <yeslee5004@gmail.com>
2022-01-14 00:42:36 +0900

    [Modify] Case

--- cpp_module_03/ex01/main.cpp
+++ cpp_module_03/ex01/main.cpp
@@ -2,23 +2,21 @@
 // Created by Yeshin Lee on 2022/01/10.
 //
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main(void) {
-    std::string const cula;
-
-    ClapTrap zizi = ClapTrap("zizi");
-    ClapTrap kongkong = ClapTrap("kongkong");
-    ClapTrap kebi = ClapTrap("kebi");
+    ClapTrap zizi("zizi");
+    ScavTrap cula("cula");
+    ScavTrap frankie("frankie");
 
     std::cout << std::endl;
-    zizi.attack(kebi.getName());
-    kebi.takeDamage(zizi.getHitPoints());
+    zizi.attack(cula.getName());
+    cula.guardGate();
 
     std::cout << std::endl;
-    kebi.beRepaired(5);
-    kongkong.attack(kebi.getName());
-    zizi.takeDamage(kongkong.getHitPoints());
-    kebi.beRepaired(3);
+    cula.beRepaired(5);
+    frankie.attack(cula.getName());
+    zizi.takeDamage(frankie.getHitPoints());
+    cula.beRepaired(3);
     std::cout << std::endl;
 }
