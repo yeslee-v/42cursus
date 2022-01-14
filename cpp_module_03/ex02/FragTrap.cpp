9ed2926d44a04b558ac8b3470b8aac5e38a6cb01
Yeshin Lee <yeslee5004@gmail.com>
2022-01-14 13:04:46 +0900

    [Add] FragTrap.cpp
    1. OCCF
    2. highFivesGuys()

---
+++ cpp_module_03/ex02/FragTrap.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Yeshin Lee on 2022/01/14.
+//
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap() {
+    this->hitPoints = 100;
+    this->energyPoints = 100;
+    this->attackDamage = 100;
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name) {
+    std::cout << "[FragTrap] Default constructor called" << std::endl;
+    this->hitPoints = 100;
+    this->energyPoints = 100;
+    this->attackDamage = 100;
+}
+
+FragTrap::FragTrap(const FragTrap &fragTrap) {
+    std::cout << "[FragTrap] Copy constructor called" << std::endl;
+    *this = fragTrap;
+
+}
+
+FragTrap& FragTrap::operator=(const FragTrap &fragTrap) {
+    std::cout << "[FragTrap] Assignation operator called" << std::endl;
+    this->name = fragTrap.getName();
+    this->hitPoints = fragTrap.getHitPoints();
+    this->energyPoints = fragTrap.getEnergyPoints();
+    this->attackDamage = fragTrap.getAttackDamage();
+    return *this;
+}
+
+FragTrap::~FragTrap() {
+    std::cout << "[FragTrap] Destructor called" << std::endl;
+}
+
+void FragTrap::highFivesGuys(void) {
+    std::cout << "[FragTrap] " << this->name << " says, Let's highfive!" << std::endl;
+}
