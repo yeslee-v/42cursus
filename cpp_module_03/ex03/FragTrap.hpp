7f86bcea764ef8605126c615e75382732c9920e2
Yeshin Lee <yeslee5004@gmail.com>
2022-01-15 00:14:14 +0900

    [Add] FragTrap to virtual ClapTrap

---
+++ cpp_module_03/ex03/FragTrap.hpp
@@ -0,0 +1,22 @@
+//
+// Created by Yeshin Lee on 2022/01/14.
+//
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : virtual public ClapTrap {
+public:
+    FragTrap();
+    FragTrap(std::string name);
+    FragTrap(const FragTrap& fragTrap);
+    FragTrap& operator=(const FragTrap& fragTrap);
+    ~FragTrap();
+
+    void highFivesGuys(void);
+};
+
+
+#endif
