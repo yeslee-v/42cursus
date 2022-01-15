fe4fa2e1e5836ffcb98ba0096f7b5fd07b05a61f
Yeshin Lee <yeslee5004@gmail.com>
2022-01-16 00:52:20 +0900

    [Add] main for test

--- cpp_module_04/ex00/main.cpp
+++ cpp_module_04/ex00/main.cpp
@@ -2,17 +2,30 @@
 // Created by Yeshin Lee on 2022/01/15.
 //
 
-#include "Animal.hpp"
-#include "WrongAnimal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
+    const Animal* animal = new Animal();
+    const Animal* dog = new Dog();
+    const Animal* cat = new Cat();
+    const WrongCat caramel;
+
+    std::cout << std::endl;
+    std::cout << dog->getType() << " " << std::endl;
+    std::cout << cat->getType() << " " << std::endl;
+    std::cout << caramel.getType() << " " << std::endl;
+
+    std::cout << std::endl;
+    animal->makeSound();
+    dog->makeSound();
+    cat->makeSound();
+    caramel.makeSound();
+
+    std::cout << std::endl;
+    delete animal;
+    delete dog;
+    delete cat;
 }
