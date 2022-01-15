d7cf2ab3a32b8d84e4e57b1dcf7a2d0543c169d2
Yeshin Lee <yeslee5004@gmail.com>
2022-01-16 00:56:01 +0900

    [Add] WrongAnimal
    1. OCCF
    2. makeSound()

--- cpp_module_04/ex00/WrongAnimal.hpp
+++ cpp_module_04/ex00/WrongAnimal.hpp
@@ -8,11 +8,18 @@
 #include <iostream>
 
 class WrongAnimal {
+protected:
+    std::string type;
 public:
     WrongAnimal();
-    WrongAnimal(type);
     WrongAnimal(const WrongAnimal& wrongAnimal);
     WrongAnimal& operator=(const WrongAnimal& wrongAnimal);
+    ~WrongAnimal();
+
+    void setType(std::string type);
+    std::string getType(void) const;
+
+    void makeSound(void) const;
 };
 
 
