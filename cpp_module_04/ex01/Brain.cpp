aaa4cffbbc79da833c5c521fc2a241d739378fc8
Yeshin Lee <yeslee5004@gmail.com>
2022-01-17 22:55:35 +0900

    [Init] Brain
    1. idx(current array index), ideas array
    2. OFFD
    3. setter, getter

---
+++ cpp_module_04/ex01/Brain.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Yeshin Lee on 2022/01/16.
+//
+
+#include "Brain.hpp"
+
+Brain::Brain() : idx(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Brain::Brain(const Brain &brain) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = brain;
+}
+
+Brain& Brain::operator=(const Brain &brain) {
+    std::cout << "Assignment copy operator called" << std::endl;
+    this->idx = brain.idx;
+    for (int i = 0; i < 100; i++)
+        this->ideas[i] = brain.ideas[i];
+    return *this;
+}
+
+Brain::~Brain() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+void Brain::setIdea(std::string idea) {
+    if (this->idx < 0 || this->idx > 100)
+    {
+        std::cout << "Wrong idx" << std::endl;
+        return ;
+    }
+    this->ideas[idx] = idea;
+    idx++;
+}
+
+std::string Brain::getIdea(unsigned int idx) const {
+    return this->ideas[idx];
+}
---
+++ cpp_module_04/ex01/Brain.hpp
@@ -0,0 +1,25 @@
+//
+// Created by Yeshin Lee on 2022/01/16.
+//
+
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+
+class Brain {
+protected:
+    int idx;
+    std::string ideas[100];
+public:
+    Brain();
+    Brain(const Brain& brain);
+    Brain& operator=(const Brain& brain);
+    ~Brain();
+
+    void setIdea(std::string idea);
+    std::string getIdea(unsigned int idx) const;
+};
+
+
+#endif
