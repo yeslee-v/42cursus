c3f346590a73d72e1a4284a4badc30a2d72ce5bc
Yeshin Lee <yeslee5004@gmail.com>
2022-01-18 14:14:45 +0900

    [Add] printing address

--- cpp_module_04/ex01/main.cpp
+++ cpp_module_04/ex01/main.cpp
@@ -4,28 +4,48 @@
 
 #include "Dog.hpp"
 #include "Cat.hpp"
-#include "WrongCat.hpp"
 
 int main()
 {
-    const Animal* animal = new Animal();
-    const Animal* dog = new Dog();
-    const Animal* cat = new Cat();
-    const WrongCat caramel;
+    {
+        int i = 20;
+        const Animal* dog = new Dog();
+        const Animal* cat = new Cat();
+
+        std::cout << std::endl;
+        std::cout << "[address]" << std::endl;
+        std::cout << "dog: " << dog << std::endl;
+        std::cout << "cat: " << cat << std::endl;
+        std::cout << std::endl;
+
+        delete dog;//should not create a leak
+        delete cat;
+        while (--i > -1)
+            std::cout << "-";
+        std::cout << std::endl;
+    }
+    int num = 4;
+    const Animal* animal[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        if (i % 2)
+        {
+            animal[i] = new Dog();
+            std::cout << "animal[" << i << "] type: " << animal[i]->getType() << std::endl;
+            std::cout << "animal[" << i << "]: address " << animal[i] << std::endl;
+            std::cout << std::endl;
+        }
+        else
+        {
+            animal[i] = new Cat();
+            std::cout << "animal[" << i << "] type: " << animal[i]->getType() << std::endl;
+            std::cout << "animal[" << i << "] address: " << animal[i] << std::endl;
+            std::cout << std::endl;
+        }
+    }
 
     std::cout << std::endl;
-    std::cout << dog->getType() << " " << std::endl;
-    std::cout << cat->getType() << " " << std::endl;
-    std::cout << caramel.getType() << " " << std::endl;
-
-    std::cout << std::endl;
-    animal->makeSound();
-    dog->makeSound();
-    cat->makeSound();
-    caramel.makeSound();
-
-    std::cout << std::endl;
-    delete animal;
-    delete dog;
-    delete cat;
+    for (int i = 0; i < num; i++)
+        delete animal[i];
 }
