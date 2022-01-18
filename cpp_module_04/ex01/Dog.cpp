c37440cde262ea190a9a630feb05ecb46f7da495
Yeshin Lee <yeslee5004@gmail.com>
2022-01-18 14:26:22 +0900

    [Add] Dog
    1. brain attribute in private
    2. setter, getter

--- cpp_module_04/ex01/Dog.cpp
+++ cpp_module_04/ex01/Dog.cpp
@@ -4,12 +4,12 @@
 
 #include "Dog.hpp"
 
-Dog::Dog() : Animal() {
+Dog::Dog() : Animal(), brain(new Brain()) {
     this->type = "Dog";
     std::cout << this->type << ": Default constructor called" << std::endl;
 }
 
-Dog::Dog(const Dog &dog) {
+Dog::Dog(const Dog &dog) : Animal(dog), brain(new Brain()) {
     std::cout << "Copy constructor called" << std::endl;
     *this = dog;
 }
@@ -17,11 +17,25 @@ Dog::Dog(const Dog &dog) {
 Dog& Dog::operator=(const Dog &dog) {
     std::cout << "Assignment copy operator called" << std::endl;
     this->type = dog.getType();
+    *(this->brain) = dog.getBrain();
     return *this;
 }
 
 Dog::~Dog() {
-    std::cout << "Destructor called" << std::endl;
+    std::cout << this->type << ": Destructor called" << std::endl;
+    delete this->brain;
+}
+
+Brain& Dog::getBrain(void) const {
+    return *(this->brain);
+}
+
+void Dog::setBrainIdea(std::string idea) {
+    this->brain->setIdea(idea);
+}
+
+std::string Dog::getBrainIdea(unsigned int idx) const {
+    return this->brain->getIdea(idx);
 }
 
 void Dog::makeSound(void) const {
--- cpp_module_04/ex01/Dog.hpp
+++ cpp_module_04/ex01/Dog.hpp
@@ -6,15 +6,22 @@
 #define DOG_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Dog : public Animal {
+private:
+    Brain* brain;
 public:
     Dog();
     Dog(const Dog& dog);
     Dog& operator=(const Dog& dog);
     ~Dog();
 
-    virtual void makeSound(void) const;
+    Brain& getBrain(void) const;
+    void setBrainIdea(std::string idea);
+    std::string getBrainIdea(unsigned int idx) const;
+
+    void makeSound(void) const;
 };
 
 
