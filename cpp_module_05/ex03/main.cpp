a373a87fb51385149e4daea2fdacba694e140e0a
Yeshin Lee <yeslee5004@gmail.com>
2022-01-21 02:14:04 +0900

    [Modify] ex03

--- cpp_module_05/ex03/main.cpp
+++ cpp_module_05/ex03/main.cpp
@@ -2,105 +2,45 @@
 // Created by Yeshin Lee on 2022/01/18.
 //
 
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
-
-void ShrubberyCreationFormTest() {
-    std::cout << "[ Case 1: ShrubberyCreationForm ]" << std::endl;
-
-    Bureaucrat alpha("alpha", 84);
-    ShrubberyCreationForm SCForm("SCForm");
-
-    std::cout << std::endl;
-    try
-    {
-        alpha.executeForm(SCForm);
-    }
-    catch (std::exception & e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << std::endl;
-    try
-    {
-        alpha.signForm(SCForm);
-        alpha.executeForm(SCForm);
-    }
-    catch (std::exception & e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-void RobotomyRequestFormTest() {
-    std::cout << "[ Case 2: RobotomyRequestForm ]" << std::endl;
-
-    RobotomyRequestForm RRForm("RRForm");
-    Bureaucrat beta("beta", 42);
-
-    std::cout << std::endl;
-    try
-    {
-        beta.executeForm(RRForm);
-    }
-    catch (std::exception & e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << std::endl;
-    try
-    {
-        beta.signForm(RRForm);
-        beta.executeForm(RRForm);
-    }
-    catch (std::exception & e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    std::cout << std::endl;
-}
-
-void PresidentialPardonFormTest() {
-    std::cout << "[ Case 3: PresidentialPardonForm ]" << std::endl;
-
-    PresidentialPardonForm PPForm("PPForm");
-    Bureaucrat delta("delta", 2);
-
-    std::cout << std::endl;
-    try
-    {
-        delta.executeForm(PPForm);
-    }
-    catch (std::exception & e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << std::endl;
-    try
-    {
-        delta.signForm(PPForm);
-        delta.executeForm(PPForm);
-    }
-    catch (std::exception & e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << std::endl;
-}
-
+#include "Intern.hpp"
+#include "Form.hpp"
 
 int main(void) {
-    ShrubberyCreationFormTest();
+
+    Intern someRandomIntern;
+
+    Form* scf;
+    Form* rrf;
+    Form* ppf;
+
     std::cout << std::endl;
-    RobotomyRequestFormTest();
+    try {
+        scf = someRandomIntern.makeForm("shrubberyCreation", "Bender");
+    }
+    catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
     std::cout << std::endl;
-    PresidentialPardonFormTest();
+    try {
+        rrf = someRandomIntern.makeForm("robotomyRequest", "Bender");
+    }
+    catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+    try {
+        ppf = someRandomIntern.makeForm("presidentialPardon", "Bender");
+    }
+    catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+    delete scf;
+    delete rrf;
+    delete ppf;
 
     return 0;
 }
