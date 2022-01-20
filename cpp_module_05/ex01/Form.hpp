0a89f23825a09a2eb337f363ba07b2be253111eb
Yeshin Lee <yeslee5004@gmail.com>
2022-01-20 17:55:54 +0900

    [Add] Form
    1. OFFD
    2. getter
    3. beSigned
    4. two class for std::exception
    5. override operator<<

---
+++ cpp_module_05/ex01/Form.hpp
@@ -0,0 +1,55 @@
+//
+// Created by Yeshin Lee on 2022/01/20.
+//
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+#include <exception>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+enum check_signed {
+    NOT_SIGNED,
+    SIGNED
+};
+
+typedef struct s_grade {
+    int required;
+    int execute;
+} t_grade;
+
+class Form {
+private:
+    std::string name;
+    bool is_signed;
+    t_grade grade;
+public:
+    Form(std::string name, int required, int execute);
+    Form(const Form& form);
+    Form& operator=(const Form& form);
+    ~Form();
+
+    std::string getName() const;
+    int getIsSigned() const;
+    int getGradeRequired() const;
+    int getGradeExecute() const;
+
+    void beSigned(const Bureaucrat& bureaucrat);
+
+    class GradeTooHighException : public std::exception {
+    public:
+        virtual const char* what() const throw();
+    };
+    class GradeTooLowException : public std::exception {
+    public:
+        virtual const char* what() const throw();
+    };
+
+};
+
+std::ostream& operator<<(std::ostream& out, const Form& form);
+
+#endif
