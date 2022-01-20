66341a144d3f5c9e524ec7452aa541042bf684b6
Yeshin Lee <yeslee5004@gmail.com>
2022-01-21 02:15:36 +0900

    [Add] Intern
    1. OFFD
    2. makeForm()
    3. NotValidFormName
    3. ValidFormName()

--- cpp_module_05/ex03/Intern.cpp
+++ cpp_module_05/ex03/Intern.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "Intern.hpp"
+
+Intern::Intern() {
+    std::cout << "Intern: default constructor called" << std::endl;
+}
+
+Intern::Intern(const Intern &intern) {
+    *this = intern;
+}
+
+Intern& Intern::operator=(const Intern &intern) {
+    return *this;
+}
+
+Intern::~Intern() {
+    std::cout << "Intern: destructor called" << std::endl;
+}
+
+int ValidFormName(std::string name) {
+    std::string FormName[3] = {
+            "shrubberyCreation",
+            "robotomyRequest",
+            "presidentialPardon"
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (FormName[i] == name)
+            return i;
+    }
+    return -1;
+}
+
+Form* Intern::makeForm(std::string name, std::string target) {
+    Form* form;
+    int value = ValidFormName(name);
+
+    switch (value) {
+        case SC:
+            form = new ShrubberyCreationForm(target);
+            break;
+        case RR:
+            form = new RobotomyRequestForm(target);
+            break;
+        case PP:
+            form = new PresidentialPardonForm(target);
+            break;
+        default:
+            throw Intern::NotValidFormException();
+    }
+    std::cout << "Intern create " << name << std::endl;
+    return form;
+}
+
+const char* Intern::NotValidFormException::what() const throw() {
+    return "Invalid Form";
+}
--- cpp_module_05/ex03/Intern.hpp
+++ cpp_module_05/ex03/Intern.hpp
@@ -2,13 +2,37 @@
 // Created by Yeshin Lee on 2022/01/21.
 //
 
-#ifndef INC_42CURSUS_INTERN_HPP
-#define INC_42CURSUS_INTERN_HPP
+#ifndef INTERN_HPP
+#define INTERN_HPP
 
+#include <iostream>
+#include <exception>
+
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+enum ExistForm {
+    SC,
+    RR,
+    PP,
+    };
 
 class Intern {
+public:
+    Intern();
+    Intern(const Intern& intern);
+    Intern& operator=(const Intern& intern);
+    ~Intern();
 
+    Form* makeForm(std::string name, std::string target);
+
+    class NotValidFormException : public std::exception {
+    public:
+        virtual const char* what() const throw();
+    };
 };
 
+int ValidFormName(std::string name);
 
-#endif //INC_42CURSUS_INTERN_HPP
+#endif
