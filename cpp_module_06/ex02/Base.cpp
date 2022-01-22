5b71766ef10002df6faccf208e316cc12bab8ac1
Yeshin Lee <yeslee5004@gmail.com>
2022-01-23 01:59:19 +0900

    [Init] Base only has public virtual destructor

---
+++ cpp_module_06/ex02/Base.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Yeshin Lee on 2022/01/22.
+//
+
+#include "Base.hpp"
+
+Base::~Base() {}
---
+++ cpp_module_06/ex02/Base.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Yeshin Lee on 2022/01/22.
+//
+
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base {
+public:
+    virtual ~Base();
+};
+
+#endif
