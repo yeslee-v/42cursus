067db4d598e5cf357179c04eabb984602afdde20
Yeshin Lee <yeslee5004@gmail.com>
2022-01-22 22:12:28 +0900

    [Init] main for test

---
+++ cpp_module_06/ex01/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Yeshin Lee on 2022/01/22.
+//
+
+#include <iostream>
+
+#include "Serialization.hpp"
+
+int main() {
+    Data info;
+    uintptr_t serialization_info;
+    Data* deserialization_inf ;
+
+    info.id = 1;
+    info.name = "yeslee";
+
+    serialization_info = serialize(&info);
+    std::cout << "serialization: " << serialization_info << std::endl;
+
+    deserialization_info =  deserialize(serialization_info);
+    std::cout << "deserialization: " << deserialization_info->name << std::endl;
+    std::cout << "deserialization: " << deserialization_info->id << std::endl;
+}
