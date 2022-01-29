3974ff7f649d33365ed90d3722684a2abd35fc5c
Yeshin Lee <yeslee5004@gmail.com>
2022-01-29 22:04:49 +0900

    [Init] ex01

---
+++ cpp_module_08/ex01/span.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yeshin Lee on 2022/01/29.
+//
+
+#include "span.hpp"
---
+++ cpp_module_08/ex01/span.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Yeshin Lee on 2022/01/29.
+//
+
+#ifndef INC_42CURSUS_SPAN_HPP
+#define INC_42CURSUS_SPAN_HPP
+
+
+class span {
+
+};
+
+
+#endif //INC_42CURSUS_SPAN_HPP
