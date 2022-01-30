f7c03e8c2418197739bd51ed2a01d0ba08573e51
Yeshin Lee <yeslee5004@gmail.com>
2022-01-30 23:24:56 +0900

    [Add] container template using vector

--- cpp_module_08/ex00/easyfind.hpp
+++ cpp_module_08/ex00/easyfind.hpp
@@ -2,7 +2,21 @@
 // Created by Yeshin Lee on 2022/01/29.
 //
 
-#ifndef INC_42CURSUS_EASYFIND_HPP
-#define INC_42CURSUS_EASYFIND_HPP
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
 
-#endif //INC_42CURSUS_EASYFIND_HPP
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template<typename T>
+T easyfind(std::vector<T> container, int value) {
+    typename std::vector<T>::iterator itr;
+
+    itr = std::find(container.begin(), container.end(), value);
+    if (itr == container.end())
+        throw std::exception();
+    return std::distance(container.begin(), itr);
+}
+
+#endif
