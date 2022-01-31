3d9716f57f15a9866a788c099fae675fd00821fe
Yeshin Lee <yeslee5004@gmail.com>
2022-01-31 23:32:31 +0900

    [Add] test for MAX value

--- cpp_module_08/ex01/main.cpp
+++ cpp_module_08/ex01/main.cpp
@@ -6,18 +6,35 @@
 
 int main()
 {
-    try {
-//        Span sp = Span(4); // too low
-        Span sp = Span(5);
-        sp.addNumber(5);
-        sp.addNumber(3);
-        sp.addNumber(17);
-        sp.addNumber(9);
-        sp.addNumber(11);
-        std::cout << sp.shortestSpan() << std::endl;
-        std::cout << sp.longestSpan() << std::endl;
+    {
+        std::cout << "[ Test in subject ]" << std::endl;
+        try {
+            Span sp = Span(5);
+            sp.addNumber(5);
+            sp.addNumber(3);
+            sp.addNumber(17);
+            sp.addNumber(9);
+            sp.addNumber(11);
+            std::cout << sp.shortestSpan() << std::endl;
+            std::cout << sp.longestSpan() << std::endl;
+        }
+        catch (std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
     }
-    catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
+
+    {
+        std::cout << "[ Test MAX num: 1000 ]" << std::endl;
+        try {
+            Span sp = Span(MAX);
+            for (int i = 0; i < MAX; i++)
+                sp.addNumber(i * 10);
+            std::cout << sp.shortestSpan() << std::endl;
+            std::cout << sp.longestSpan() << std::endl;
+        }
+        catch (std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
     }
+
 }
