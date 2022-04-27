993ff6dcbcb93ef471ed99dfbf213ac3e9ec1d83
Yeshin Lee <yeslee5004@gmail.com>
2022-04-27 11:48:37 +0900

    [Modify] modifier: assign, push_back, pop_back

--- ft_containers/Vector.hpp
+++ ft_containers/Vector.hpp
@@ -10,177 +10,240 @@
 
 namespace ft {
     template < typename T, typename Alloc = std::allocator<T> >
-    class vector {
-    public:
-        typedef T                                       value_type;
-        typedef Alloc                                   allocator_type;
-        typedef Alloc::reference                        reference;
-        typedef Alloc::const_reference                  const_reference;
-        typedef Alloc::pointer	                        pointer;
-        typedef Alloc::const_pointer	                const_pointer;
-        typedef ft::random_access_iterator<T>           iterator;
-        typedef ft:random_access_iterator<const T>      const_iterator;
-        typedef ft::reverse_iterator<iterator>          reverse_iterator;
-        typedef ft::reverse_iterator<const_iterator>    const_reverse_iterator;
-        typedef ptrdiff_t                               difference_type;
-        typedef size_t                                  size_type;
+            class vector {
+            public:
+                typedef T                                       value_type;
+                typedef Alloc                                   allocator_type;
+                typedef Alloc::reference                        reference;
+                typedef Alloc::const_reference                  const_reference;
+                typedef Alloc::pointer	                        pointer;
+                typedef Alloc::const_pointer	                const_pointer;
+                typedef ft::random_access_iterator<T>           iterator;
+                typedef ft:random_access_iterator<const T>      const_iterator;
+                typedef ft::reverse_iterator<iterator>          reverse_iterator;
+                typedef ft::reverse_iterator<const_iterator>    const_reverse_iterator;
+                typedef ptrdiff_t                               difference_type;
+                typedef size_t                                  size_type;
 
-    private:
-        allocator_type  _alloc;
-        size_type       _n; // size
-        size_type       _cap;
-        value_type*     _val;
-        value_type*     _first;
-        value_type*     _last;
+            private:
+                allocator_type  _alloc;
+                size_type       _n; // size
+                size_type       _cap;
+                value_type*     _val;
+                value_type*     _first;
+                value_type*     _last;
 
-    public:
-        explicit vector(const allocator_type& alloc = allocator_type())
-        : _alloc(alloc), _n(0), _cap(0) {
-            _val = _alloc.allocate(0);
-            _first = _val;
-            _last = _val;
-        };
+            public:
+                explicit vector(const allocator_type& alloc = allocator_type())
+                : _alloc(alloc), _n(0), _cap(0) {
+                    _val = _alloc.allocate(0);
+                    _first = _val;
+                    _last = _val;
+                };
 
-        explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
-        : _alloc(alloc), _n(n), _cap(0) {
-            while (_cap < _n) {
-                size_type new_cap = (_cap * 2 > 0) ? (_cap * 2) : 1;
-                _cap = new_cap;
-            }
-            _val = alloc.allocate(_cap);
-            _first = _val;
-            _last = _first;
-            for (n--) {
-                _alloc.construct(_last, val);
-                _last++;
-            }
-        };
+                explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+                : _alloc(alloc), _n(n), _cap(0) {
+                    while (_cap < _n) {
+                        size_type new_cap = (_cap * 2 > 0) ? (_cap * 2) : 1;
+                        _cap = new_cap;
+                    }
+                    _val = alloc.allocate(_cap);
+                    _first = _val;
+                    _last = _first;
+                    for (n--) {
+                        _alloc.construct(_last, val);
+                        _last++;
+                    }
+                };
 
-        template <class InputIterator> // iterator 공부하고 나서 진행해야
-        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
-        : _alloc(alloc) {
-        };
+                template <class InputIterator> // iterator 공부하고 나서 진행해야
+                        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+                        : _alloc(alloc) {
+                        };
 
-        vector(const vector& x) {
-            *this = x;
-        };
+                vector(const vector& x) {
+                    *this = x;
+                };
 
-        vector& operator=(const vector& vector) {
-            _alloc = vector._alloc;
-            _n = vector._n;
-            _cap = vector._cap;
-            _val = _alloc.allocate(_cap);
-            _first = _val;
-            _last = _first;
-            for (n--) {
-                _alloc.construct(_last, val);
-                _last++;
-            }
-        };
+                vector& operator=(const vector& vector) {
+                    _alloc = vector._alloc;
+                    _n = vector._n;
+                    _cap = vector._cap;
+                    _val = _alloc.allocate(_cap);
+                    _first = _val;
+                    _last = _first;
+                    for (n--) {
+                        _alloc.construct(_last, val);
+                        _last++;
+                    }
+                };
 
-        ~vector() {
-            clear();
-            _alloc.deallocate(_val, _cap);
-        };
+                ~vector() {
+                    clear();
+                    _alloc.deallocate(_val, _cap);
+                };
 
 
-        /*
-         * Iterators
-         */
-        iterator begin() {
-            return iterator(_first);
-        }
+                /*
+                 * Iterators
+                 */
+                iterator begin() {
+                    return iterator(_first);
+                }
 
-        iterator end() {
-            return iterator(_last);
-        }
+                iterator end() {
+                    return iterator(_last);
+                }
 
-        reverse_iterator rbegin() {
-            return reverse_iterator(end());
-        }
+                reverse_iterator rbegin() {
+                    return reverse_iterator(end());
+                }
 
-        reverse_iterator rend() {
-            return reverse_iterator(begin());
-        }
+                reverse_iterator rend() {
+                    return reverse_iterator(begin());
+                }
 
-        /*
-         * Capacity
-         */
-        size_type size() const {
-            return _n;
-        }
+                /*
+                 * Capacity
+                 */
+                size_type size() const {
+                    return _n;
+                }
 
-        size_type max_size() const {
-            return Alloc.max_size();
-        }
+                size_type max_size() const {
+                    return _alloc.max_size();
+                }
 
-        void resize(size_type n, value_type val = value_type()) {
-            if (max_size() < n)
-                throw (std::length_error("vector::resize"))
-            else if (n < _n) {
-                clear();
-                for (size_type i = 0; i < n; i++)
-                    _alloc.construct(_last, val);
-            }
-            else if (_n < n) {
-                while (n - _n)
-                    _alloc.construct(_last, val);
-            }
-            else if (_cap < n) {
-                _alloc.deallocate(_first, _val);
-                while (n--)
-                    _alloc.allocate(_first, val);
-            }
-        }
+                void resize(size_type n, value_type val = value_type()) {
+                    if (max_size() < n)
+                        throw (std::length_error("vector::resize"))
+                        else if (n < _n) {
+                            clear();
+                            for (size_type i = 0; i < n; i++)
+                                _alloc.construct(_last, val);
+                        }
+                    else if (_n < n) {
+                        while (n - _n)
+                            _alloc.construct(_last, val);
+                    }
+                    else if (_cap < n) {
+                        _alloc.deallocate(_first, _val);
+                        while (n--)
+                            _alloc.allocate(_first, val);
+                    }
+                }
 
-        size_type capacity() const {
-            return _cap;
-        }
+                size_type capacity() const {
+                    return _cap;
+                }
 
-        bool empty() const {
-            return (_n > 0) ? 1 : 0;
-        }
+                bool empty() const {
+                    return (_n > 0) ? 1 : 0;
+                }
 
-        void reserve(size_type n) {
-            if (max_size() < n)
-                std::length_error("vector::reserve");
-            _alloc.deallocate(_first, _val);
-            for (size_type i = 0; i < n, i++)
-                _alloc.allocate(_first, i);
-        }
+                void reserve(size_type n) {
+                    if (max_size() < n)
+                        std::length_error("vector::reserve");
+                    _alloc.deallocate(_first, _val);
+                    for (size_type i = 0; i < n, i++)
+                        _alloc.allocate(_first, i);
+                }
 
-//        Element access:
-//        operator[]
-//        Access element (public member function )
-//        at
-//        Access element (public member function )
-//        front
-//        Access first element (public member function )
-//        back
-//        Access last element (public member function )
-//        data
-//        Access data (public member function )
-//
-//        Modifiers:
-//        assign
-//        Assign vector content (public member function )
-//        push_back
-//        Add element at the end (public member function )
-//        pop_back
-//        Delete last element (public member function )
-//        insert
-//        Insert elements (public member function )
-//        erase
-//        Erase elements (public member function )
-//        swap
-//        Swap content (public member function )
-//        clear
-//        Clear content (public member function )
-//        emplace
-//        Construct and insert element (public member function )
-//        emplace_back
-//        Construct and insert element at the end (public member function )
-//
+                /*
+                 * Element access
+                 */
+                reference operator[] (size_type n) {
+                    return reference(_first + n);
+                }
+
+                const reference operator[] (size_type n) const {
+                    return reference(_first + n);
+                }
+
+                reference at(size_type n) {
+                    if (_cap < n)
+                        throw std::out_of_range(std::at);
+                    return reference(_first + n);
+                }
+
+                const_reference at(size_type n) const {
+                    if (_cap < n)
+                        throw std::out_of_range(std::at);
+                    return reference(_first + n);
+                }
+
+                reference front() {
+//                    if (_first == _last)
+                        // undefined behavior
+                    return _first;
+                }
+
+                const_reference front() const {
+                    // if (_first == _last)
+                    // undefined behavior
+                    return _first;
+                }
+
+                reference back() {
+                    return _last;
+                }
+
+                const_reference back() const {
+                    return _last;
+                }
+
+                /*
+                 * Modifiers
+                 */
+                template<class InputIterator>
+                void assign(InputIteratorfirst, InputIterator last) {
+                }
+
+                void assign(size_type n, const value_type& val) {
+                    _alloc.allocate(n);
+                    for (size_type i = 0; i < n; ++i) {
+                        _alloc.construct(_last, val);
+                        _last++;
+                    }
+                }
+
+                void push_back(const value_type& val) {
+                    if (_cap < _n + 1) {
+                        ~vector();
+                        for (size_type i = 0; i <= _n; ++i) {
+                            _alloc.allocate(_last, _val);
+                        }
+                    }
+                    vector[_last + 1] = val; // 위치를 어떻게 잡아야하지?
+                }
+                void pop_back() {
+                    _alloc.destroy(_last);
+                    if (_first == _last) {
+                        _alloc.deallocate();
+                    }
+                }
+                iterator insert(iterator position, const value_type& val) {
+                    if (_cap < _n + 1)
+                        resize(_n + 1, val);
+                    for (std::vector<T>::iterator it = vector.begin(); it < vector.end(); ++it) {
+                        if (it == position)
+                            *it = val;
+                        // 뒤로 한 칸씩 미루기
+                    }
+                }
+                void insert(iterator position, size_type n, const value_type& val) {
+
+                }
+                template <class InputIterator>
+                        void insert(iterator position, InputIterator first, InputIterator last) {
+                        }
+
+                erase
+                swap
+                clear
+                emplace
+                emplace_back
+
 //        Allocator:
 //        get_allocator
 //        Get allocator (public member function )
