a1b0079b753615c8c6d28a9059a70161eb9828ae
yeslee-v <yeslee5004@gmail.com>
2022-05-30 21:04:57 +0900

    [Bug] use of       overloaded operator '==' is ambiguous       (with operand types 'ft::stack<int>'       and 'ft::stack<int>')

--- ft_containers/stack.hpp
+++ ft_containers/stack.hpp
@@ -1,44 +1,64 @@
 #ifndef STACK_HPP
-# define STACK_HPP
+#define STACK_HPP
 
 #include "vector.hpp"
 
-namespace ft {
-	template <typename T, class Container = ft::stack<T>>
-	class stack {
-		public:
-			typedef typename Container::value_type	value_type;
-			typedef typename Container::size_type	size_type;
-			typedef			 Container				container_type;
-		protected:
-			Container c;
-		
-		public:
-			//	member function
-			explicit stack (const container_type& ctnr = container_type());	
-			bool empty() const { return c.empty(); };
-			size_type size() const { return c.size(); };
-			value_type& top() { return c.back(); };
-			const value_type& top() const { return c.back(); };	
-			void push (const value_type& val) { return c.push_back(); };
-			void pop() { return c.pop_back(); };
+namespace ft
+{
+template <typename T, class Container = ft::vector<T> >
+class stack
+{
+public:
+	typedef typename Container::value_type value_type;
+	typedef typename Container::size_type size_type;
+	typedef Container container_type;
 
-			// non-member function overloads
-			template <class T, class Container>
-				bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
-			template <class T, class Container>
-				bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
-			template <class T, class Container>
-				bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
-			template <class T, class Container>
-				bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
-			template <class T, class Container>
-				bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
-			template <class T, class Container>
-				bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
-	}
+public:
+	//	member function
+	explicit stack(const container_type &ctnr = container_type()) : c(ctnr){};
+	bool empty() const { return c.empty(); };
+	size_type size() const { return c.size(); };
+	value_type &top() { return c.back(); };
+	const value_type &top() const { return c.back(); };
+	void push(const value_type &val) { return c.push_back(val); };
+	void pop() { return c.pop_back(); };
 
+protected:
+	Container c;
 
-}	
+private:
+	template <typename U, class _Cont>
+	friend bool operator==(const stack<T, _Cont> &lhs, const stack<U, _Cont> &rhs);
+	
+	template <typename U, class _Cont>
+	friend bool operator<(const stack<T, _Cont> &lhs, const stack<U, _Cont> &rhs);
+
+	template <typename U, class _Cont>
+	friend bool operator>(const stack<T, _Cont> &lhs, const stack<U, _Cont> &rhs);
+
+};
+
+// non-member function overloads
+// non-member function에서 friend를 이용해 protected, private에 접근한다 
+
+template <typename U, class Cont>
+bool operator==(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs) { return lhs.c == rhs.c; };
+
+template <typename U, class Cont>
+bool operator!=(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs) { return !(lhs == rhs); };
+
+template <typename U, class Cont>
+bool operator<(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs) { return lhs.c < rhs.c; };
+
+template <typename U, class Cont>
+bool operator<=(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs) { return !(lhs > rhs); };
+
+template <typename U, class Cont>
+bool operator>(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs) { return lhs.c > rhs.c; };
+
+template <typename U, class Cont>
+bool operator>=(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs) { return !(lhs < rhs); };
+
+} // namespace ft
 
 #endif
