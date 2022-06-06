14c532fdc529296e73cb8ccaa32f26b58c58303e
Yeshin Lee <yeslee5004@gmail.com>
2022-06-06 14:56:52 +0900

    [Feat] binary_search_tree.hpp

--- ft_containers/test.cpp
+++ ft_containers/test.cpp
@@ -1,11 +1,93 @@
 #include <map>
 #include <iostream>
+#include "utils/binary_search_tree.hpp"
+
+using namespace std;
 
 int main(void) {
-    std::map<int, int> m;
+    ft::Bst<int, std::less<int>, std::allocator<int> > bst;
 
-    std::cout << (m.find(1) != m.end() ? "찾음" : "못찾음") << "\n";
-    m[1] = 10;
-    std::cout << m.begin()->first << " " << m.begin()->second << std::endl;
+//    bst.insertNode(8);
+//    bst.insertNode(5);
+//    bst.insertNode(6);
+//
+//    bst.deleteNode(5);
+//    bst.print();
+        bst.insertNode(10);
+        bst.print();
+        cout << "------" << endl;
+
+        bst.deleteNode(10);
+        bst.print();
+        cout << "--------------------" << endl;
+
+        bst.insertNode(10);
+        bst.insertNode(8);
+        bst.print();
+        cout << "------" << endl;
+
+        bst.deleteNode(10);
+        bst.print();
+        cout << "---------------------------" << endl;
+    //
+        bst.insertNode(10);
+        bst.print();
+        cout << "------" << endl;
+
+        bst.deleteNode(8);
+        bst.print();
+        cout << "---------------------------" << endl;
+    //
+        bst.insertNode(8);
+        bst.print();
+        cout << "------" << endl;
+
+        bst.deleteNode(8);
+        bst.print();
+        cout << "-------------------------------" << endl;
+    //
+        bst.insertNode(11);
+        bst.print();
+        cout << "------" << endl;
+
+        bst.deleteNode(11);
+        bst.print();
+        cout << "---------------------------" << endl;
+    //
+        bst.insertNode(5);
+        bst.insertNode(6);
+        bst.insertNode(7);
+        bst.insertNode(8);
+        bst.print();
+        cout << "----------------------------------------------------------------" << endl;
+
+        bst.deleteNode(7);
+        bst.print();
+        cout << "------" << endl;
+        bst.deleteNode(10); // delete root
+        bst.print();
+        cout << "------" << endl;
+        bst.deleteNode(5);
+        bst.print();
+        bst.deleteNode(6);
+        bst.print();
+    cout << "-------------------------------" << endl;
+    //
+        bst.insertNode(4);
+        bst.insertNode(7);
+        bst.insertNode(6);
+        bst.insertNode(5);
+        bst.print();
+        cout << "------" << endl;
+
+        bst.deleteNode(8);
+        bst.print();
+        cout << "---------------------------" << endl;
+
+//    std::map<int, int> m;
+//
+//    std::cout << (m.find(1) != m.end() ? "찾음" : "못찾음") << "\n";
+//    m[1] = 10;
+//    std::cout << m.begin()->first << " " << m.begin()->second << std::endl;
     return (0);
 }
